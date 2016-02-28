f1b5ecc17bf9b6b1b61de5f6324424ad92518c1c
Tom Gillespie <tgbugs@gmail.com>
2016-02-28 15:49:01 -0800

    some changes for the ttl conversion

--- convert_imports.sh
+++ convert_imports.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cd ~/git/NIF-Ontology/ttl/
-#find . -type l -exec unlink {} \;
-find ../ -name '*.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
+find . -type l -exec unlink {} \;
+find ../ -name '*owl.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
+#find ../ -name '*owl.ttl' -exec sh -c 'echo $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
 
 #FILENAME=~/git/NIF-Ontology/ttl/nif.ttl
 #FN2=~/git/NIF-Ontology/ttl/NIF-Cell.ttl
@@ -11,4 +12,16 @@ find ../ -name '*.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed 
 FILENAMES=$(find . -type l -name '*.ttl')
 # fails when encounter owl:imports
 #sed --follow-symlinks -i "s/\(^\s\+<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAME $FN2
+
+declare -A subs
+subs[Backend]="<http:\/\/ontology.neuinfo.org\/NIF\/Backend\/"
+subs[p15]="<http:\/\/ontology.neuinfo.org\/NIF\/Backend\/"
+subs[nif_back]="<http:\/\/ontology.neuinfo.org\/NIF\/Backend\/"
+subs[p16]="<http:\/\/ontology.neuinfo.org\/NIF\/BiomaterialEntities\/"
+
+for FIND in "${!subs[@]}"; do
+    sed --follow-symlinks -i "/@prefix\ ${FIND}/d" $FILENAMES
+    sed --follow-symlinks -i "s/${FIND}:\(.\+\)\ /${subs[$FIND]}\1> /" $FILENAMES
+done
+
 sed --follow-symlinks -i "s/\(<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAMES
--- gather_owls.sh
+++ gather_owls.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env sh
-BASE=~/git/NIF-Ontology/
+BASE=~/git/NIF-Ontology
 find "${BASE}"/Backend -name '*.owl' > /tmp/back_files
 find "${BASE}"/BiomaterialEntities/ -name '*.owl' > /tmp/be_files
 find "${BASE}"/DigitalEntities/ -name '*.owl' > /tmp/de_files
 find "${BASE}"/Dysfunction/ -name '*.owl' > /tmp/dys_files
 find "${BASE}"/Function/ -name '*.owl' > /tmp/fun_files
-find "${BASE}" -name 'nif.owl' > /tmp/base_files
+#find "${BASE}" -name 'nif.owl' > /tmp/base_files
+
+#find "${BASE}"/*/ -name '*.owl.ttl' -exec rm {} \;
+
 java -cp ~/git/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App /tmp/*_files
 ./convert_imports.sh
+
 # gonna need to fix/improve how convert_imports works to deal with curified stuff
+
+#dupes
+#diff $(find . | grep BIRNLex-OBI-proxy.owl)  # OK check iri refs
+
+#gvim $(find . | grep quality.owl)
+#gvim $(find . | grep NIF-Resource.owl)
+#gvim $(find . | grep quality_bfo_bridge.owl)
+#gvim $(find . | grep NIF-Dysfunction-DOID-Bridge.owl)
+#gvim $(find . | grep NIF-Quality.owl)
+
+# external imports (do not ttl them!)
+#CogPO.owl  # megawtf here
+#so.owl #aka sequence.owl  # seriously wtf moving this to external import
+#uberon.owl
+#ero.owl
+#pr.owl  # FXIME
+#go.owl
+#
