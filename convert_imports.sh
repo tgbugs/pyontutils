301a924c1a085a0648fdd78010e6cc4f5d98b287
Tom Gillespie <tgbugs@gmail.com>
2016-03-03 00:09:29 -0800

    updated ttl conversion shellscripts, may need to break them up and move the code around a bit given that the method has changed a bit

--- convert_imports.sh
+++ convert_imports.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env bash
+BASE=~/git/NIF-Ontology
+BASESC=$(echo ${BASE} | sed 's/\//\\\//g')
+PATHTARGET=${BASE}/ttl/
 
-cd ~/git/NIF-Ontology/ttl/
-find . -type l -exec unlink {} \;
-find ../ -name '*owl.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
+#cd ${BASE}/ttl/
+#find . -type l -exec unlink {} \;
+#find ../ -name '*owl.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
 #find ../ -name '*owl.ttl' -exec sh -c 'echo $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
+
+OWLFILES=$(find ${BASE} -name '*.owl')
+#echo $OWLFILES
+GOOD=$(cat ${BASE}/convert.txt ${BASE}/bridge.txt | sed "s/^/${BASESC}\//")
+EXTERNAL=$(cat ${BASE}/external.txt | sed "s/^/${BASESC}\//")
+for OWLFILE in ${OWLFILES}; do
+    TARGET=$(basename $OWLFILE | sed 's/\.owl/.ttl/')
+    if [[ $GOOD =~ $OWLFILE ]]; then
+        echo 'mv' ${OWLFILE}.ttl $PATHTARGET$TARGET
+        mv ${OWLFILE}.ttl $PATHTARGET$TARGET
+    elif [[ $EXTERNAL =~ $OWLFILE ]]; then
+        echo 'cp -a' $OWLFILE ${BASE}/external/
+        cp -a $OWLFILE ${BASE}/external/
+        echo 'rm' $OWLFILE.ttl 
+        rm $OWLFILE.ttl 
+    # TODO put Views and Retired in utility?
+    else
+        echo 'DEAD mv' $OWLFILE.ttl ${PATHTARGET}unused/${TARGET}
+        mv $OWLFILE.ttl ${PATHTARGET}unused/${TARGET}
+    fi
+done
 
 #FILENAME=~/git/NIF-Ontology/ttl/nif.ttl
 #FN2=~/git/NIF-Ontology/ttl/NIF-Cell.ttl
 #FILENAMES=~/git/NIF-Ontology/ttl/NIF-Function.ttl
 
-FILENAMES=$(find . -type l -name '*.ttl')
+cd $PATHTARGET
+#FILENAMES=$(find . -type l -name '*.ttl')
+FILENAMES=$(find . -type f -name '*.ttl')
 # fails when encounter owl:imports
 #sed --follow-symlinks -i "s/\(^\s\+<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAME $FN2
 
@@ -25,3 +51,7 @@ for FIND in "${!subs[@]}"; do
 done
 
 sed --follow-symlinks -i "s/\(<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAMES
+
+# not the right place to do this, though it does seem to work
+#GITHUB="https:\/\/raw.githubusercontent.com\/SciCrunch\/NIF-Ontology\/ttl\/"
+#sed --follow-symlinks -i "s/\(<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/<${GITHUB}ttl\3.ttl>/" $FILENAMES
--- gather_owls.sh
+++ gather_owls.sh
@@ -1,27 +1,41 @@
 #!/usr/bin/env sh
 BASE=~/git/NIF-Ontology
+BASESC=$(echo ${BASE} | sed 's/\//\\\//g')
+echo $BASESC
+CONVFILE=/tmp/owl_to_convert.txt
+
 find "${BASE}"/Backend -name '*.owl' > /tmp/back_files
 find "${BASE}"/BiomaterialEntities/ -name '*.owl' > /tmp/be_files
 find "${BASE}"/DigitalEntities/ -name '*.owl' > /tmp/de_files
 find "${BASE}"/Dysfunction/ -name '*.owl' > /tmp/dys_files
 find "${BASE}"/Function/ -name '*.owl' > /tmp/fun_files
-#find "${BASE}" -name 'nif.owl' > /tmp/base_files
+find "${BASE}"/Views/ -name '*.owl' > /tmp/view_files
+find "${BASE}"/Retired/ -name '*.owl' > /tmp/re_files
+find "${BASE}" -name 'nif.owl' > /tmp/base_files
 
-#find "${BASE}"/*/ -name '*.owl.ttl' -exec rm {} \;
+find "${BASE}"/*/ -name '*.owl.ttl' -exec rm {} \;  # XXX this is EXTREMELY dangerous to copy and paste
 
 java -cp ~/git/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App /tmp/*_files
-./convert_imports.sh
+
+
+#sed "s/^/${BASESC}\//" ${BASE}/convert.txt > ${CONVFILE}
+#sed "s/^/${BASESC}\//" ${BASE}/bridge.txt >> ${CONVFILE}
+#java -cp ~/git/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App ${CONVFILE}
+
+#./convert_imports.sh
 
 # gonna need to fix/improve how convert_imports works to deal with curified stuff
 
 #dupes
 #diff $(find . | grep BIRNLex-OBI-proxy.owl)  # OK check iri refs
 
-#gvim $(find . | grep quality.owl)
-#gvim $(find . | grep NIF-Resource.owl)
-#gvim $(find . | grep quality_bfo_bridge.owl)
-#gvim $(find . | grep NIF-Dysfunction-DOID-Bridge.owl)
-#gvim $(find . | grep NIF-Quality.owl)
+#gvim $(find . | grep quality.owl)  # both deleted (replaced by pato)
+#gvim $(find . | grep quality_bfo_bridge.owl)  # deleted BE and ./ versions
+#gvim $(find . | grep NIF-Dysfunction-DOID-Bridge.owl)  # both deleted
+
+#crazytown below :/ one will go in the main and one in the unused and we can sort it out after that
+#gvim $(find . | grep NIF-Quality.owl)  # identical, the issue is that the BE version iri is used in the bridge
+#gvim $(find . | grep NIF-Resource.owl)  # complete an utter madness between BE and DE
 
 # external imports (do not ttl them!)
 #CogPO.owl  # megawtf here
