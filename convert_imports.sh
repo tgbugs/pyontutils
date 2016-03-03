5b1b90b9d29b4263d3ed19c086270fe9985267d6
Tom Gillespie <tgbugs@gmail.com>
2016-03-03 14:04:30 -0800

    first pass at sorting the files out into their proper folders is probably a bad idea, thus we need to finish dealing with the duplicate files and then do the ttl converstion so that we don't conflate conversion with sorting into the new file structure

--- convert_imports.sh
+++ convert_imports.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 BASE=~/git/NIF-Ontology
 BASESC=$(echo ${BASE} | sed 's/\//\\\//g')
-PATHTARGET=${BASE}/ttl/
+TTLTARGET=${BASE}/ttl/
+EXTERNALTARGET=${BASE}/external/
+UNUSEDTARGET=${BASE}/ttl/unused/
+TMPUNUSED=/tmp/temp_unused
 
 #cd ${BASE}/ttl/
 #find . -type l -exec unlink {} \;
 #find ../ -name '*owl.ttl' -exec sh -c 'ln -sT $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
 #find ../ -name '*owl.ttl' -exec sh -c 'echo $1 $(echo $1 | sed "s/\.owl//" | sed "s/^.\+\///")' _ {} \;
 
+echo > $TMPUNUSED
 OWLFILES=$(find ${BASE} -name '*.owl')
 #echo $OWLFILES
 GOOD=$(cat ${BASE}/convert.txt ${BASE}/bridge.txt | sed "s/^/${BASESC}\//")
@@ -15,8 +19,8 @@ EXTERNAL=$(cat ${BASE}/external.txt | sed "s/^/${BASESC}\//")
 for OWLFILE in ${OWLFILES}; do
     TARGET=$(basename $OWLFILE | sed 's/\.owl/.ttl/')
     if [[ $GOOD =~ $OWLFILE ]]; then
-        echo 'mv' ${OWLFILE}.ttl $PATHTARGET$TARGET
-        mv ${OWLFILE}.ttl $PATHTARGET$TARGET
+        echo 'mv' ${OWLFILE}.ttl $TTLTARGET$TARGET
+        mv ${OWLFILE}.ttl $TTLTARGET$TARGET
     elif [[ $EXTERNAL =~ $OWLFILE ]]; then
         echo 'cp -a' $OWLFILE ${BASE}/external/
         cp -a $OWLFILE ${BASE}/external/
@@ -24,16 +28,19 @@ for OWLFILE in ${OWLFILES}; do
         rm $OWLFILE.ttl 
     # TODO put Views and Retired in utility?
     else
-        echo 'DEAD mv' $OWLFILE.ttl ${PATHTARGET}unused/${TARGET}
-        mv $OWLFILE.ttl ${PATHTARGET}unused/${TARGET}
+        echo 'DEAD mv' $OWLFILE.ttl ${TTLTARGET}unused/${TARGET}
+        mv $OWLFILE.ttl ${TTLTARGET}unused/${TARGET}
+        echo ${TTLTARGET}unused/${TARGET} >> /tmp/temp_unused
     fi
 done
+
+cat $TMPUNUSED
 
 #FILENAME=~/git/NIF-Ontology/ttl/nif.ttl
 #FN2=~/git/NIF-Ontology/ttl/NIF-Cell.ttl
 #FILENAMES=~/git/NIF-Ontology/ttl/NIF-Function.ttl
 
-cd $PATHTARGET
+cd $TTLTARGET
 #FILENAMES=$(find . -type l -name '*.ttl')
 FILENAMES=$(find . -type f -name '*.ttl')
 # fails when encounter owl:imports
@@ -45,12 +52,26 @@ subs[p15]="<http:\/\/ontology.neuinfo.org\/NIF\/Backend\/"
 subs[nif_back]="<http:\/\/ontology.neuinfo.org\/NIF\/Backend\/"
 subs[p16]="<http:\/\/ontology.neuinfo.org\/NIF\/BiomaterialEntities\/"
 
-for FIND in "${!subs[@]}"; do
-    sed --follow-symlinks -i "/@prefix\ ${FIND}/d" $FILENAMES
-    sed --follow-symlinks -i "s/${FIND}:\(.\+\)\ /${subs[$FIND]}\1> /" $FILENAMES
-done
+if true; then
+    for FIND in "${!subs[@]}"; do
+        sed --follow-symlinks -i "/@prefix\ ${FIND}/d" $FILENAMES
+        sed --follow-symlinks -i "s/${FIND}:\(.\+\)\ /${subs[$FIND]}\1> /" $FILENAMES
+    done
 
-sed --follow-symlinks -i "s/\(<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAMES
+    sed --follow-symlinks -i "s/\(<http:\/\/ontology\.neuinfo\.org\/NIF\/\)\(.\+\)\(\/[_0-9A-Za-z\-]\+\)\(\.owl>\)/\1ttl\3.ttl>/" $FILENAMES
+fi
+for FPATH in $(cat ${BASE}/external.txt); do
+    ONTFN=$(basename $FPATH)
+    TTLFN=$(echo $ONTFN | sed "s/.owl$/.ttl/")
+    #echo $TTLFN
+    #echo $ONTFN
+    sed -i "s/\/ttl\/${TTLFN}/\/external\/${ONTFN}/" $FILENAMES
+done
+for FPATH in $(cat ${TMPUNUSED}); do
+    TTLFN=$(basename $FPATH)
+    echo $TTLFN
+    sed -i "s/\/ttl\/${TTLFN}/\/ttl\/unused\/${TTLFN}/" $FPATH
+done
 
 # not the right place to do this, though it does seem to work
 #GITHUB="https:\/\/raw.githubusercontent.com\/SciCrunch\/NIF-Ontology\/ttl\/"
