19a4a9d7157ed8c6a85eea27e6ef255c33b4117b
Tom Gillespie <tgbugs@gmail.com>
2016-03-16 17:28:55 -0700

    starting work on NIF-Neuron.ttl and NIF-Neuron-phenotypes.ttl

---
+++ nif_neuron.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import rdflib
+import csv
+
+id_ = None
+
+(id_, rdflib.RDF.type, rdflib.OWL.Class)
+(id_, rdflib.RDFS.subClassOf, 'NIFCELL:sao1417703748')
+
+syntax = '{region}{layer_or_subregion}{expression}{ephys}{molecular}{morph}{cellOrNeuron}'
+
