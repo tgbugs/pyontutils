f5239e2a74e836111b64c107484b4c89a4d7c19f
Tom Gillespie <tgbugs@gmail.com>
2016-03-18 20:09:36 -0700

    work on merging nif quality with the hbp phenotypes

--- nif_neuron.py
+++ nif_neuron.py
@@ -2,10 +2,14 @@
 import io
 import os
 import csv
+from urllib.parse import quote
 import rdflib
+from IPython import embed
 from scr_sync import makeGraph
 from obo_io import OboFile
-from IPython import embed
+from scigraph_client import Graph
+
+sgg = Graph(quiet=False)
 
 id_ = None
 
@@ -23,22 +27,29 @@ with open('neuron_phenotype_edges.csv', 'rt') as f:
     rows = [r for r in csv.reader(f)]
 
 
-with open('neurons.csv', 'rt') as f:
-    nrows = [r for r in csv.reader(f)]
+#with open('neurons.csv', 'rt') as f:
+    #nrows = [r for r in csv.reader(f)]
 
 PREFIXES = {
     #'ILX':'http://uri.interlex.org/base/ilx_',
     '':'http://FIXME.org/',
     'ilx':'http://uri.interlex.org/base/',
     'skos':'http://www.w3.org/2004/02/skos/core#',
+    'owl':'http://www.w3.org/2002/07/owl#',
+    'dc':'http://purl.org/dc/elements/1.1/',
+    'nsu':'http://www.FIXME.org/nsupper#',
+
     'OBOANN':'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#',  # FIXME needs to die a swift death
-    'NIFQUAL':'http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#'
+    'NIFQUAL':'http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#',
+
 }
 #g = makeGraph('NIF-Neuron-phenotypes', prefixes=PREFIXES)
 g = makeGraph('NIF-Neuron', prefixes=PREFIXES)
 
 for row in rows[1:]:
     if row[0].startswith('#') or not row[0]:
+        if row[0] == '#references':
+            break
         print(row)
         continue
     id_ = PREFIXES['ilx'] + row[0]
@@ -56,11 +67,15 @@ g.write()
 
 #phenotype sources
 neuroner = '~/git/neuroNER/resources/bluima/neuroner/hbp_morphology_ontology.obo'
+neuroner1 = '~/git/neuroNER/resources/bluima/neuroner/hbp_electrophysiology_ontology.obo'
+neuroner2 = '~/git/neuroNER/resources/bluima/neuroner/hbp_electrophysiology-triggers_ontology.obo'
 hbp_cell = ''
 nif_qual = '~/git/NIF-Ontology/ttl/NIF-Quality.ttl'
 
 mo = OboFile(os.path.expanduser(neuroner))
-mo_ttl = mo.__ttl__()
+mo1 = OboFile(os.path.expanduser(neuroner1))
+mo2 = OboFile(os.path.expanduser(neuroner2))
+mo_ttl = mo.__ttl__() + mo1.__ttl__() + mo2.__ttl__()
 
 mo_ttl = """\
 @prefix : <http://FIXME.org/> .
@@ -76,8 +91,104 @@ ng = rdflib.Graph()
 ng.parse(data=mo_ttl, format='turtle')
 ng.parse(os.path.expanduser(nif_qual), format='turtle')
 #ng.namespace_manager.bind('default1', None, override=False, replace=True)
-#embed()
+ng.remove((None, rdflib.OWL.imports, None))
+
+exact = []
+similar = []
+quals = []
+s2 = {}
+
+for subject, label in sorted(ng.subject_objects(rdflib.RDFS.label)):
+    #print(subject)
+    #print(label.lower())
+    if 'quality' in label.lower():
+        quals.append((subject, label))
+    subpre = ng.namespace_manager.compute_qname(subject)[1]
+    llower = rdflib.Literal(label.lower(), lang='en')
+    for s in ng.subjects(rdflib.RDFS.label, llower):
+        if s != subject:
+            exact.append((subject, s, label, llower))
+    for s, p, o in sorted(ng.triples((None, rdflib.RDFS.label, None))):
+        spre = ng.namespace_manager.compute_qname(s)[1]
+        if subject != s and label.lower() in o.lower().split(' ') and spre != subpre:
+            print()
+            print(spre, subpre)
+            similar.append((subject, s, label, o))
+            if subpre.toPython() == 'http://FIXME.org/':
+                print('YAY')
+                print(label, ',', o)
+                print(subject, s)
+                subject, s = s, subject
+                label, o = o, label
+
+            s2[subject] = {'label': label.toPython(), 'o': o.toPython(), 'xrefs':[subject, s]}  # FIXME overwrites
+
+print()
+for k, v in s2.items():
+    print(k)
+    for k, v2 in sorted(v.items()):
+        print('    ', k, ':', v2)
+
+desired_nif_terms = set()
+starts = [
+#"NIFQUAL:sao2088691397",
+#"NIFQUAL:sao1278200674",
+#"NIFQUAL:sao2088691397",
+#"NIFQUAL:sao-1126011106",  # FIXME WTF IS THIS NONSENSE  (scigraph bug?)
+quote("http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#sao1959705051").replace('/','%2F'),
+quote("http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#sao2088691397").replace('/','%2F'),
+quote("http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#sao1278200674").replace('/','%2F'),
+quote("http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#sao2088691397").replace('/','%2F'),
+quote("http://ontology.neuinfo.org/NIF/BiomaterialEntities/NIF-Quality.owl#sao-1126011106").replace('/','%2F'),
+]
+
+for id_ in starts:
+    want = sgg.getNeighbors(id_, relationshipType='subClassOf', direction='INCOMING', depth=5)
+    #print(id_, want)
+    desired_nif_terms.update([n['id'] for n in want['nodes']])
+
+print(desired_nif_terms)
+
+new_terms = {}
+for s, o in sorted(ng.subject_objects(rdflib.RDFS.label))[::-1]:
+    spre = ng.namespace_manager.compute_qname(s)[1]
+    if s in new_terms:
+        print(s, 'already in as xref probably')
+        continue
+    elif spre.toPython() != 'http://FIXME.org/' and s.toPython() not in desired_nif_terms:
+        print('DO NOT WANT', s)
+        continue
+
+    data = {}
+    id_ = ilx_base.format(ilx_start)
+    ilx_start += 1
+    if s in s2:
+        d = s2[s]
+        new_terms[d['xrefs'][0]] = {'replaced_by':id_}
+        new_terms[d['xrefs'][1]] = {'replaced_by':id_}
+
+        data['labels'] = [d['label'], d['o']]
+        data['xrefs'] = d['xrefs']
+    else:
+        new_terms[s] = {'replaced_by':id_}
+        data['labels'] = [o.toPython()]
+        data['xrefs'] = [s]
+
+    new_terms[id_] = data
+
+def pprint(thing):
+    for t in thing:
+        print()
+        for v in t:
+            print(v)
+
+#pprint(exact)
+#pprint(similar)
+#pprint(quals)
+
+embed()
 g2 = makeGraph('pheno-comp', PREFIXES)
 for t in ng.triples((None, None, None)):
     g2.add_node(*t)  # only way to clean prefixes :/
+
 g2.write()
