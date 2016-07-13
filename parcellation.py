3a76f739568689e6558253b48fc34a7e0c93b350
Tom Gillespie <tgbugs@gmail.com>
2016-07-13 16:53:45 -0700

    more work on parcellation

--- parcellation.py
+++ parcellation.py
@@ -3,26 +3,27 @@
 from collections import namedtuple
 import rdflib
 from pyontutils.utils import makeGraph
+from IPython import embed
 
 ONT_PATH = 'http://ontology.neuinfo.org/NIF/ttl/'
 
 PScheme = namedtuple('PScheme', ['curie', 'name', 'species', 'devstage', 'citation'])
+
 schemes = [
-PScheme('ILX:','','','',''),
-PScheme('ILX:','CoCoMac parcellation concept','NCBITaxon:9544','various','problem'),  # problems detected :/
-PScheme('ILX:','Allen Mouse Brain Atlas parcellation concept','NCBITaxon:10090','adult P56','http://help.brain-map.org/download/attachments/2818169/AllenReferenceAtlas_v2_2011.pdf?version=1&modificationDate=1319667383440'),  # yay no doi! wat
+PScheme('ilx:empty','','','',''),
+PScheme('ilx:1','CoCoMac parcellation concept','NCBITaxon:9544','various','problem'),  # problems detected :/
+PScheme('ilx:2','Allen Mouse Brain Atlas parcellation concept','NCBITaxon:10090','adult P56','http://help.brain-map.org/download/attachments/2818169/AllenReferenceAtlas_v2_2011.pdf?version=1&modificationDate=1319667383440'),  # yay no doi! wat
 ]
 
-root = ('ILX:', 'Brain parcellation scheme concept')
-
+root = ('ilx:butts', 'Brain parcellation scheme concept')
 
 def make_scheme(parent, scheme):  # ick...
     out = [
         (scheme.curie, rdflib.RDF.type, rdflib.OWL.Class),
-        (scheme.curie, rdflib.RDFS.label, scheme.name)
-        (scheme.curie, 'OBOANN:', scheme.species)
-        (scheme.curie, 'OBOANN', scheme.devstage)
-        (scheme.curie, 'OBOANN:definingCitation', scheme.citation)
+        (scheme.curie, rdflib.RDFS.label, scheme.name),
+        (scheme.curie, 'ilx:has_taxon', scheme.species),
+        (scheme.curie, 'ilx:has_developmental_stage', scheme.devstage),
+        (scheme.curie, 'OBOANN:definingCitation', scheme.citation),
     ]
     return out
 
@@ -30,6 +31,8 @@ def main():
     filename = 'parcellation'
     ontid = ONT_PATH + filename + 'ttl'
     PREFIXES = {
+        'ilx':'http://uri.interlex.org/base/',
+        'NCBITaxon':'http://purl.obolibrary.org/obo/NCBITaxon_',
         'skos':'http://www.w3.org/2004/02/skos/core#',
         'OBOANN':'http://ontology.neuinfo.org/NIF/Backend/OBO_annotation_properties.owl#',  # FIXME needs to die a swift death
                }
@@ -37,9 +40,10 @@ def main():
     new_graph.add_node(ontid, rdflib.RDF.type, rdflib.OWL.Ontology)
     new_graph.add_node(ontid, rdflib.RDFS.label, filename)
     new_graph.add_node(ontid, rdflib.RDFS.comment, 'Brain parcellation schemes as represented by root concepts.')
+    parent = root[0]
     for scheme in schemes:
         s = make_scheme(parent, scheme)
-        [new_graph.add_node(t) for t in s]
+        [new_graph.add_node(*t) for t in s]
 
     new_graph.write()
 
