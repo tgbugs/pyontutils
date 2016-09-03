6d163daec30c670040d0fbcfacd58c5e0b2ac40e
Tom Gillespie <tgbugs@gmail.com>
2016-09-03 16:05:02 -0700

    scr_sync removed old local version of makeGraph

--- scr_sync.py
+++ scr_sync.py
@@ -91,55 +91,6 @@ def make_records(resources, res_cols, field_mapping):
 
     return output
 
-class makeGraph:
-    def __init__(self, name, prefixes):
-        self.name = name
-        self.namespaces = {p:rdflib.Namespace(ns) for p, ns in prefixes.items()}
-        self.g = rdflib.Graph()
-        [self.g.namespace_manager.bind(p, ns) for p, ns in prefixes.items()]
-
-    def write(self):
-        write_loc = '/tmp/ttl_files'
-        with open('/tmp/' + self.name + '.ttl', 'wb') as f:
-            f.write(self.g.serialize(format='turtle'))
-        with open(write_loc, 'wt') as f:
-            f.write('/tmp/' + self.name + '.ttl\n')
-        os.system('java -cp ' +
-            os.path.expanduser('~/git/ttl-convert/target/'
-                               'ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar') +
-                  ' scicrunch.App ' + write_loc)
-
-    def expand(self, curie):
-        #print(curie)
-        prefix, suffix = curie.split(':')
-        if prefix not in self.namespaces:
-            raise KeyError('Namespace prefix does exist:', prefix)
-        return self.namespaces[prefix][suffix]
-
-    def check_thing(self, thing):
-        if type(thing) != rdflib.term.URIRef:
-            try:
-                return self.expand(thing)
-            except (KeyError, ValueError) as e:
-                if thing.startswith('http') and ' ' not in thing:  # so apparently some values start with http :/
-                    return rdflib.URIRef(thing)
-                else:
-                    raise TypeError('Unknown format:', thing)
-        else:
-            return thing
-
-    def add_node(self, target, edge, value):
-        target = self.check_thing(target)
-        edge = self.check_thing(edge)
-        try:
-            if value.startswith(':') and ' ' in value:  # not a compact repr AND starts with a : because humans are insane
-                value = ' ' + value
-            value = self.check_thing(value)
-        except (TypeError, AttributeError) as e:
-            value = rdflib.Literal(value)  # trust autoconv
-        self.g.add( (target, edge, value) )
-
-
 field_to_edge = {
     'abbrev':'obo_annot:abbrev',
     'alt_id':'oboInOwl:hasDbXref',
