287421129825ffc3b67f58d3eebe003a93266511
Tom Gillespie <tgbugs@gmail.com>
2016-11-04 02:03:11 -0700

    ttlfmt added process pool executor for zooooom

--- ttlfmt.py
+++ ttlfmt.py
@@ -2,45 +2,50 @@
 """Format ontology files using a uniform ttl serializer from rdflib
 
 Usage:
-    ttlfmt [-a] <file>...
+    ttlfmt [options] <file>...
 
 Options:
     -h --help       print this
     -v --verbose    do something fun!
     -a --vanilla    use the regular rdflib turtle serializer
+    -s --slow       do not use a process pool
 
 """
 import os
 from docopt import docopt
 import rdflib
-from IPython import embed
+from concurrent.futures import ProcessPoolExecutor
+args = docopt(__doc__, version = "ttlfmt 0")
+
+if args['--vanilla']:
+    outfmt = 'turtle'
+else:
+    outfmt = 'nifttl'
+
 
 rdflib.plugin.register('nifttl', rdflib.serializer.Serializer, 'pyontutils.ttlser', 'CustomTurtleSerializer')
 
-def main():
-    args = docopt(__doc__, version = "ttlfmt 0")
-    #print(args)
-
-    if args['--vanilla']:
-        outfmt = 'turtle'
+def convert(file):
+    filepath = os.path.expanduser(file)
+    _, ext = os.path.splitext(filepath)
+    filetype = ext.strip('.')
+    if filetype == 'ttl':
+        infmt = 'turtle'
     else:
-        outfmt = 'nifttl'
+        infmt = None
+    print(filepath)
+    graph = rdflib.Graph()
+    graph.parse(filepath, format=infmt)
+    out = graph.serialize(format=outfmt)
+    with open(filepath, 'wb') as f:
+        f.write(out)
 
-    for file in args['<file>']:
-        filepath = os.path.expanduser(file)
-        _, ext = os.path.splitext(filepath)
-        filetype = ext.strip('.')
-        if filetype == 'ttl':
-            infmt = 'turtle'
-        else:
-            infmt = None
-        print(filepath)
-        graph = rdflib.Graph()
-        graph.parse(filepath, format=infmt)
-        out = graph.serialize(format=outfmt)
-        with open(filepath, 'wb') as f:
-            f.write(out)
-    return
+def main():
+    if args['--slow'] or len(args['<file>']) == 1:
+        [convert(f) for f in args['<file>']]
+    else:
+        with ProcessPoolExecutor(4) as ppe:
+            futures = [ppe.submit(convert, _) for _ in args['<file>']]
 
 if __name__ == '__main__':
     main()
