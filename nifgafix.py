2348d603726f2098c87ed0131b1676ce459fac83
Tom Gillespie <tgbugs@gmail.com>
2016-12-15 15:58:45 -0800

    nifgafix added ontology line

--- nifgafix.py
+++ nifgafix.py
@@ -7,6 +7,8 @@ PREFIXES = makePrefixes('NIFGA', 'NIFSTD', 'owl')
 
 g = rdflib.Graph()
 g.parse('http://purl.obolibrary.org/obo/uberon/bridge/uberon-bridge-to-nifstd.owl', format='xml')
-ng = makeGraph('NIFGA-Equivs', PREFIXES)
+name = 'NIFGA-Equivs'
+ng = makeGraph(name, PREFIXES)
 [ng.g.add(t) for t in ((rdflib.URIRef(PREFIXES['NIFGA'] + o.rsplit('/',1)[-1]), p, o) for s, p, o in g.triples((None, rdflib.OWL.equivalentClass, None)))]
+ng.add_ont('http://ontology.neuinfo.org/NIF/ttl/generated/' + name + '.ttl', 'NIFGA to NIFSTD mappings')
 ng.write()
