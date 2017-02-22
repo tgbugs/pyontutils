2e14e1e4840427535d752b914e2ef7dad28d0913
Tom Gillespie <tgbugs@gmail.com>
2017-02-22 01:26:27 -0800

    added nif_load to print the nif import chain

---
+++ nif_load.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.5
+""" Run in NIF-Ontology/ttl/ """
+from glob import glob
+import rdflib
+from pyontutils.utils import makeGraph, makePrefixes  # TODO make prefixes needs an all...
+from pyontutils.hierarchies import creatTree
+from collections import namedtuple
+from IPython import embed
+
+Query = namedtuple('Query', ['root','relationshipType','direction','depth'])
+
+graph = rdflib.Graph()
+
+for f in glob('*/*.ttl') + glob('*.ttl'):
+    print(f)
+    graph.parse(f, format='turtle')
+
+mg = makeGraph('nifall', makePrefixes('NIFTTL', 'owl', 'skos'), graph=graph)
+j = mg.make_scigraph_json('owl:imports', direct=True)
+embed()
+t, te = creatTree(*Query('NIFTTL:nif.ttl', 'owl:imports', 'outgoing', 30), json=j)
+print(t)
