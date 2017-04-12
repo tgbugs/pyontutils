748150cd3f62ffbd5a624b7a408abf0a8b9489e6
Tom Gillespie <tgbugs@gmail.com>
2017-04-12 02:54:08 -0700

    setup now working as desired

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from setuptools import setup, find_packages
 
-# since setuptools is stupid and cannot actually exclude files...
+# since setuptools cannot actually exclude files so just grab the ones we want
 
 files = [
     'pyontutils/__init__.py',
@@ -23,40 +23,42 @@ os.mkdir('export')
 for f in files:
     shutil.copyfile(f, f.replace('pyontutils','export'))
 
-setup(
-    name='pyontutils',
-    version='0.0.1',
-    description='utilities for working with the NIFSTD ontology and SciGraph',
-    long_description=' ',
-    url='https://github.com/tgbugs/pyontutils',
-    author='Tom Gillespie',
-    author_email='tgbugs@gmail.com',
-    license='MIT',
-    classifiers=[],
-    keywords='nif nifstd ontology scigraph',
-    package_dir=['pyontutils', 'export'],
-    packages=['pyontutils'],
-    install_requires=[
-        'docopt',
-        'numpy',
-        'psycopg2',
-        'requests',
-        'ipython',
-        'rdflib',
-        'sqlalchemy',
-        'pyyaml',
-        'lxml',
-    ],
-    #extras_require
-    #package_data
-    #data_files=[('resources',['pyontutils/resources/chebi-subset-ids.txt',])],  # not part of distro
-    entry_points={
-        'console_scripts': [
-            'scigraph-codegen=pyontutils.scigraph:main',
-            'scig=pyontutils.scig:main',
-            'ttlfmt=pyontutils.ttlfmt:main',
+try:
+    setup(
+        name='pyontutils',
+        version='0.0.1',
+        description='utilities for working with the NIFSTD ontology and SciGraph',
+        long_description=' ',
+        url='https://github.com/tgbugs/pyontutils',
+        author='Tom Gillespie',
+        author_email='tgbugs@gmail.com',
+        license='MIT',
+        classifiers=[],
+        keywords='nif nifstd ontology scigraph',
+        package_dir={'pyontutils':'export'},
+        packages=['pyontutils'],
+        install_requires=[
+            'docopt',
+            'numpy',
+            'psycopg2',
+            'requests',
+            'ipython',
+            'rdflib',
+            'sqlalchemy',
+            'pyyaml',
+            'lxml',
         ],
-    },
-)
+        #extras_require
+        #package_data
+        #data_files=[('resources',['pyontutils/resources/chebi-subset-ids.txt',])],  # not part of distro
+        entry_points={
+            'console_scripts': [
+                'scigraph-codegen=pyontutils.scigraph:main',
+                'scig=pyontutils.scig:main',
+                'ttlfmt=pyontutils.ttlfmt:main',
+            ],
+        },
+    )
 
-shutil.rmtree('export')
+finally:
+    shutil.rmtree('export')
