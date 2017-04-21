dea1c65dac6e7dd1852401f0ccfeec1dad184f64
Tom Gillespie <tgbugs@gmail.com>
2017-04-20 19:30:49 -0700

    neuron example added dendrite phenotypes and changed modelling to include them, also dealt with a number of naming issues and spelling issues

--- pyontutils/neuron_example.py
+++ pyontutils/neuron_example.py
@@ -18,10 +18,11 @@ Rat = Phenotype('NCBITaxon:10116', 'ilx:hasInstanceInSpecies', label='Rattus nor
 # pyramidal phenotypes (cortex)
 P = P = PC = Phenotype('ilx:PyramidalPhenotype', pred.hasMorphologicalPhenotype)  # 
 PB = BP = BPC = Phenotype('ilx:BiopolarPyramidalPhenotype', pred.hasMorphologicalPhenotype)  # collision
+PH = HP = HPC = Phenotype('ilx:HorizontalPyramidalPhenotype', pred.hasMorphologicalPhenotype)
 PI = IP = IPC = Phenotype('ilx:InvertedPyramidalPhenotype', pred.hasMorphologicalPhenotype)
-PN = NP = NPC = Phenotype('ilx:NarrowPyramidalPhenotype', pred.hasMorphologicalPhenotype)
+PN = NP = NPC = Phenotype('ilx:NarrowPyramidalPhenotype', pred.hasMorphologicalPhenotype)  # XXX These are apparently tufted.... so are they NarrowTufted or not? help!
 PS = SP = SPC = Phenotype('ilx:StarPyramidalPhenotype', pred.hasMorphologicalPhenotype)  # collision with stratum pyramidale
-PTN = NTP = NTPC = TPC_C = Phenotype('ilx:NarrowTuftedPyramidalPhenotype', pred.hasMorphologicalPhenotype)  # are this different?
+PTN = NTP = NTPC = TPC_C = Phenotype('ilx:NarrowTuftedPyramidalPhenotype', pred.hasMorphologicalPhenotype)  # are this different?  Yes, the Narrow modifieds the tufted... not entirely sure how that is different from when Narrow modifies Pyramidal...
 PT = TP = TPC = Phenotype('ilx:TuftedPyramidalPhenotype', pred.hasMorphologicalPhenotype)
 PTS = STP = STPC = Phenotype('ilx:SlenderTuftedPyramidalPhenotype', pred.hasMorphologicalPhenotype)
 PTT = TTP = TTPC = Phenotype('ilx:ThickTuftedPyramidalPhenotype', pred.hasMorphologicalPhenotype)
@@ -34,8 +35,8 @@ PU = UP = UPC = UTPC = Phenotype('ilx:UntuftedPyramidalPhenotype', pred.hasMorph
 BT = BTC = Phenotype('ilx:BituftedPhenotype', pred.hasMorphologicalPhenotype)
 B = BP = Phenotype('ilx:BipolarPhenotype', pred.hasMorphologicalPhenotype)  # NOTE disjoint from biopolar pyramidal... also collision
 BS = Phenotype('ilx:BistratifiedPhenotype', pred.hasMorphologicalPhenotype)
-Ch = ChC = Phenotype('ilx:ChandalierPhenotype', pred.hasMorphologicalPhenotype)
-DB = DBC = Phenotype('ilx:DoubleBoquetPhenotype', pred.hasMorphologicalPhenotype)
+Ch = ChC = Phenotype('ilx:ChandelierPhenotype', pred.hasMorphologicalPhenotype)  # AA ??
+DB = DBC = Phenotype('ilx:DoubleBouquetPhenotype', pred.hasMorphologicalPhenotype)
 DA = DAC = Phenotype('ilx:DescendingAxonPhenotype', pred.hasMorphologicalPhenotype)  # FIXME how to communicate that these are a bit different than actual 'axon' phenotypes? disjoint from P?
 HA = HAC = Phenotype('ilx:HorizontalAxonPhenotype', pred.hasMorphologicalPhenotype)
 LA = LAC = Phenotype('ilx:LargeAxonPhenotype', pred.hasMorphologicalPhenotype)
@@ -58,13 +59,14 @@ b = Phenotype('ilx:PetillaInitialBurstSpikingPhenotype', pred.hasElectrophysiolo
 c = Phenotype('ilx:PetillaInitialClassicalSpikingPhenotype', pred.hasElectrophysiologicalPhenotype)
 d = Phenotype('ilx:PetillaInitialDelayedSpikingPhenotype', pred.hasElectrophysiologicalPhenotype)
 
-L1 = Phenotype('UBERON:0005390', pred.hasSomaLocatedIn)
-L2 = Phenotype('UBERON:0005391', pred.hasSomaLocatedIn)
-L3 = Phenotype('UBERON:0005392', pred.hasSomaLocatedIn)
+L1 = Phenotype('UBERON:0005390', pred.hasLayerLocationPhenotype)
+L2 = Phenotype('UBERON:0005391', pred.hasLayerLocationPhenotype)
+L3 = Phenotype('UBERON:0005392', pred.hasLayerLocationPhenotype)
 L23 = LogicalPhenotype(OR, L2, L3)  # if you try to do this inside a dict definition it will fail :/
-L4 = Phenotype('UBERON:0005393', pred.hasSomaLocatedIn)
-L5 = Phenotype('UBERON:0005394', pred.hasSomaLocatedIn)
-L6 = Phenotype('UBERON:0005395', pred.hasSomaLocatedIn)
+L23.labelPostRule = lambda l: 'L23' # heh
+L4 = Phenotype('UBERON:0005393', pred.hasLayerLocationPhenotype)
+L5 = Phenotype('UBERON:0005394', pred.hasLayerLocationPhenotype)
+L6 = Phenotype('UBERON:0005395', pred.hasLayerLocationPhenotype)
 
 L1P = Phenotype('UBERON:0005390', pred.hasProjectionPhenotype)
 L4P = Phenotype('UBERON:0005393', pred.hasProjectionPhenotype)
@@ -80,27 +82,43 @@ SLM = Phenotype('UBERON:0007640', pred.hasLayerLocationPhenotype)
 SLU = Phenotype('UBERON:0007637', pred.hasLayerLocationPhenotype)
 SR = Phenotype('UBERON:0005372', pred.hasLayerLocationPhenotype)
 
+SLMP = Phenotype('UBERON:0007640', pred.hasProjectionPhenotype)
+ThalP = Phenotype('UBERON:0001897', pred.hasProjectionPhenotype)
+
 # expression
 CB = Phenotype('PR:000004967', pred.hasExpressionPhenotype)
 CCK = Phenotype('PR:000005110', pred.hasExpressionPhenotype)
 CR = Phenotype('PR:000004968', pred.hasExpressionPhenotype)
 NPY = Phenotype('PR:000011387', pred.hasExpressionPhenotype)
-PV = Phenotype('NIFMOL:nifext_6', pred.hasExpressionPhenotype)
+PV = Phenotype('PR:000013502', pred.hasExpressionPhenotype)
 SOM = Phenotype('PR:000015665', pred.hasExpressionPhenotype)
 VIP = Phenotype('PR:000017299', pred.hasExpressionPhenotype)
+GABA = Phenotype('CHEBI:16865', pred.hasExpressionPhenotype)
+D1 = Phenotype('PR:000001175', pred.hasExpressionPhenotype)
+
+# dendrite phenotypes
+Th = Phenotype('ilx:ThickPhenotype', pred.hasDendriteMorphologicalPhenotype)
+Tu = Phenotype('ilx:TuftedPhenotype', pred.hasDendriteMorphologicalPhenotype)
+U = NegPhenotype(Tu)
+U.labelPostRule = lambda l: 'Un' + l.lower()
+S = Phenotype('ilx:SlenderPhenotype', pred.hasDendriteMorphologicalPhenotype)
+EB = Phenotype('ilx:EarlyBifurcatingPhenotype', pred.hasDendriteMorphologicalPhenotype)
+LB = Phenotype('ilx:LateBifurcatingPhenotype', pred.hasDendriteMorphologicalPhenotype)
+Sp = Phenotype('ilx:SpinyPhenotype', pred.hasDendriteMorphologicalPhenotype)
+ASp = NegPhenotype(Sp)
 
 # other?
 PPA = Phenotype('ilx:PerforantPathwayAssociated', pred.hasPhenotype)  # TODO FIXME
 
 #OLM = Phenotype('', pred.hasLayerLocationPhenotype)  # removed since it seems to be a logical of SO and SLM
 TRI = Phenotype('ilx:TrilaminarPhenotype', pred.hasMorphologicalPhenotype)
-AA = Phenotype('ilx:AxonAxonicPhenotype', pred.hasMorphologicalPhenotype)  # axo axonic??!?
+#AA = Phenotype('ilx:AxoAxonicPhenotype', pred.hasMorphologicalPhenotype)  # axo axonic??!? This is ChC
 IVY = Phenotype('ilx:IvyPhenotype', pred.hasMorphologicalPhenotype)  # check syns on this?
-IS1 = Phenotype('ilx:IS1IHaveNoIdeaPhenotype', pred.hasMorphologicalPhenotype)  # i have no idea what these are
+#IS1 = Phenotype('ilx:IS1IHaveNoIdeaPhenotype', pred.hasMorphologicalPhenotype)  # these are NOT morphological types, they are an example of a 'true type' in the sense that you need multiple meausres to confirm them
 SCA = Phenotype('GO:1990021', pred.hasProjectionPhenotype)#'ilx:hasAssociatedTractPhenotype')  # how to model these... also schaffer collaterals are a MESS in the ontology FIXME
 STRI = Phenotype('UBERON:0005383', pred.hasSomaLocatedIn)  # VS UBERON:0002435 (always comes up)
 MSN = Phenotype('ilx:MediumSpinyPhenotype', pred.hasMorphologicalPhenotype)
-DIRPATH = Phenotype('ilx:DirectPathway', pred.hasCircuitRolePhenotype)  # how to model these...  TODO FIXME
+#DIRPATH = Phenotype('ilx:DirectPathway', pred.hasCircuitRolePhenotype)  # this is fine for a generic neuron, but PR:000001175 is really what we are looking at here
 INT = Phenotype('ilx:InterneuronPhenotype', pred.hasCircuitRolePhenotype)  # unsatisfactory
 CER = Phenotype('UBERON:0002037', pred.hasSomaLocatedIn)
 GRAN = Phenotype('ilx:GranulePhenotype', pred.hasMorphologicalPhenotype)  # vs granular?
@@ -121,21 +139,21 @@ neurons = {
     'HBP_CELL:0000013': NeuronC(CCK),
     'HBP_CELL:0000016': NeuronC(PV),
     'HBP_CELL:0000018': NeuronC(PC),
-    'HBP_CELL:0000024': NeuronC(UPC),  # untufted... dendrite phenotype?? or what
+    'HBP_CELL:0000024': NeuronC(U, PC), #NeuronC(UPC),  # untufted... dendrite phenotype?? or what
     'HBP_CELL:0000025': NeuronC(SS),
     'HBP_CELL:0000027': NeuronC(SPC),
     'HBP_CELL:0000028': NeuronC(L23, PC),
-    'HBP_CELL:0000030': NeuronC(Phenotype('ilx:HorizontalPyramidalPhenotype', pred.hasMorphologicalPhenotype)),  # are phenotypes like this modifiers on _pyramidal_ or are they on the _cell_
+    'HBP_CELL:0000030': NeuronC(HPC),  # are phenotypes like this modifiers on _pyramidal_ or are they on the _cell_
     'HBP_CELL:0000031': NeuronC(BPC),  # AAAAAA
-    'HBP_CELL:0000032': NeuronC(NPC, Phenotype('UBERON:0001897', pred.hasProjectionPhenotype)),
-    'HBP_CELL:0000035': NeuronC(TPC, L4P),  # FIXME LxP
-    'HBP_CELL:0000036': NeuronC(TPC, L1P),  # FIXME LxP
-    'HBP_CELL:0000038': NeuronC(STPC),
-    'HBP_CELL:0000039': NeuronC(TTPC2),  # can we identify early/late in other cells?
-    'HBP_CELL:0000040': NeuronC(TTPC1),
+    'HBP_CELL:0000032': NeuronC(NPC, ThalP),
+    'HBP_CELL:0000035': NeuronC(Tu, PC, L4P), #NeuronC(TPC, L4P),  # looking over the 2015 paper this is ok
+    'HBP_CELL:0000036': NeuronC(Tu, PC, L1P), #NeuronC(TPC, L1P),  # looking over the 2015 paper this is ok
+    'HBP_CELL:0000038': NeuronC(S, Tu, PC), #NeuronC(STPC),
+    'HBP_CELL:0000039': NeuronC(Th, Tu, PC, EB), #NeuronC(TTPC2),  # can we identify early/late in other cells?
+    'HBP_CELL:0000040': NeuronC(Th, Tu, PC, LB), #NeuronC(TTPC1),
     'HBP_CELL:0000042': NeuronC(IPC),
-    'HBP_CELL:0000044': NeuronC(L5, TTPC),
-    'HBP_CELL:0000051': NeuronC(L6, TPC, L1P),  # FIXME LxP
+    'HBP_CELL:0000044': NeuronC(L5, Th, Tu, PC), # NeuronC(L5, TTPC),
+    'HBP_CELL:0000051': NeuronC(L6, Tu, PC, L1P), #NeuronC(L6, TPC, L1P),  # looking over the 2015 paper this is ok
     'HBP_CELL:0000053': NeuronC(BP),
     'HBP_CELL:0000054': NeuronC(BTC),
     'HBP_CELL:0000055': NeuronC(MC),
@@ -170,16 +188,16 @@ neurons = {
     'HBP_CELL:0000135': NeuronH(SLM, PPA),
     'HBP_CELL:0000136': NeuronH(SO, BP),
     'HBP_CELL:0000137': NeuronH(SPy, BS),
-    'HBP_CELL:0000138': NeuronH(LogicalPhenotype(OR, SO, SLM)),  # FIXME I assume that 'oriens lacunosum moleculare' is actually SO, SLM...
+    'HBP_CELL:0000138': NeuronH(SO, SLMP),  # see (PMID: 23042082)
     'HBP_CELL:0000139': NeuronH(SPy, TRI),  # TODO
-    'HBP_CELL:0000140': NeuronH(AA),  # FIXME AA??!? == ChC??!??!
+    'HBP_CELL:0000140': NeuronH(ChC),  # see (PMID: 8915675) AA are indeed ChC
     'HBP_CELL:0000141': NeuronH(SPy, BC),
     'HBP_CELL:0000143': NeuronH(SPy, PV, BC),
-    'HBP_CELL:0000144': NeuronH(SPy, IVY),  # TODO
+    'HBP_CELL:0000144': NeuronH(SPy, IVY),
     'HBP_CELL:0000145': NeuronH(SPy, PC),
-    'HBP_CELL:0000146': NeuronH(SPy, IS1),  # TODO
+    'HBP_CELL:0000146': NeuronH(SPy, GABA, CR),  # XXX review please IS-1 see (PMID: 8915675) page 27 these do not have distinctive morphology, they have distinctive connectivity... dendrodendritic connections within type and dendritic arbors in SR ... if these are really morphological then it is not the intrisice morphology, but the connectivity...? help?
     'HBP_CELL:0000147': NeuronH(SPy, SCA),  # TODO schafer collateral projecting?
-    'HBP_CELL:0000148': Neuron(Rat, STRI, MSN, DIRPATH),  # TODO are these really dirpath or are they D1?
+    'HBP_CELL:0000148': Neuron(Rat, STRI, MSN, D1),  # the original class was direct pathway, but that is generic
     'HBP_CELL:0000149': Neuron(Rat, CA3, PC),
     'HBP_CELL:0000151': NeuronH(INT),  # TODO
     'HBP_CELL:0000152': Neuron(Rat, CER, GRAN),  # TODO
@@ -203,6 +221,7 @@ neurons = {
 #Neuron(Phenotype())
 
 def main():
+    NeuronC(BC)
     WRITE()
     embed()
 
