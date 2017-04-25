8fa13998a1133697902c4ca2ed4111f185def447
Tom Gillespie <tgbugs@gmail.com>
2017-04-24 18:23:51 -0700

    neuron lang major reworking of how configuring the language works via a singleton Controller class

--- pyontutils/neuron_lang.py
+++ pyontutils/neuron_lang.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+from git.repo import Repo
 from rdflib import Graph, URIRef
 from pyontutils.neurons import *
 from pyontutils.utils import makeGraph, makePrefixes
@@ -7,13 +9,14 @@ from IPython import embed
 __all__ = [
     'AND',
     'OR',
-    'graphBase',
+    #'graphBase',
+    'Controller',
     'Phenotype',
     'NegPhenotype',
     'LogicalPhenotype',
     'Neuron',
-    'pred',
-    'WRITE',
+    'pred',  # set globally when Controller is instantiated
+    'WRITE',  # set globally when Controller is instantiated
     'WRITEPYTHON',
 ]
 
@@ -31,50 +34,142 @@ _ = [rename(old, new) for old, new in (
     ('LogicalPhenoEdge','LogicalPhenotype'),
 )]
 
-#core_graph_path = 'https://raw.githubusercontent.com/SciCrunch/NIF-Ontology/neurons/ttl/NIF-Phenotype-Core.ttl'
-#pheno_graph_path = 'https://raw.githubusercontent.com/SciCrunch/NIF-Ontology/neurons/ttl/NIF-Phenotypes.ttl'
-core_graph_path = '/home/tom/git/NIF-Ontology/ttl/NIF-Phenotype-Core.ttl'
-pheno_graph_path = '/home/tom/git/NIF-Ontology/ttl/NIF-Phenotypes.ttl'
 
-in_graph_path = core_graph_path #/tmp/output.ttl'
-out_graph_path =  '/tmp/_Neurons.ttl'
+class Controller:
+    """ If you pass new arguments here any Neurons
+    that you have previously defined will be wiped.
 
-graphBase.core_graph = Graph()
-graphBase.core_graph.parse(core_graph_path, format='turtle')
-graphBase.core_graph.parse(pheno_graph_path, format='turtle')
-graphBase.in_graph = graphBase.core_graph
-graphBase.in_graph.parse(in_graph_path, format='turtle')
-graphBase.in_graph.namespace_manager.bind('ILXREPLACE', makePrefixes('ILXREPLACE')['ILXREPLACE'])  # FIXME annoying
-graphBase.in_graph.namespace_manager.bind('GO', makePrefixes('GO')['GO'])  # FIXME annoying
-graphBase.in_graph.namespace_manager.bind('CHEBI', makePrefixes('CHEBI')['CHEBI'])  # FIXME annoying
-graphBase.out_graph = Graph()
-graphBase._predicates = getPhenotypePredicates(graphBase.core_graph)
-pred = graphBase._predicates  # keep the predicates in their own namespace
+    To set your own output_graph_path write
+    `Controller().output_graph_path = 'myOutFile.ttl'`
 
-newGraph = makeGraph('',
-                     prefixes=makePrefixes('owl',
-                                           'GO',
-                                           'PR',
-                                           'CHEBI',
-                                           'UBERON',
-                                           'NCBITaxon',
-                                           'ILXREPLACE',
-                                           'ilx',
-                                           'ILX',
-                                           'NIFCELL',
-                                           'NIFMOL',),
-                     graph=graphBase.out_graph)
-newGraph.filename = out_graph_path
-ontid = URIRef('file://' + out_graph_path)
-newGraph.add_ont(ontid, 'Some Neurons')
-newGraph.add_node(ontid, 'owl:imports', URIRef(pheno_graph_path))  # core should be in the import closure
+    NOTE: if you change your core files you will need
+    to redefine pred.
+    """
+    instance = None
+    class __Controller:
+        remote_base = 'https://raw.githubusercontent.com/SciCrunch/NIF-Ontology/'
+        local_base = os.path.expanduser('~/git/NIF-Ontology/')
+        branch = 'neurons'
+        def __init__(self, *args,
+                     core_graph_paths=['ttl/NIF-Phenotype-Core.ttl',
+                                       'ttl/NIF-Phenotypes.ttl'],
+                     in_graph_paths=tuple(),
+                     out_graph_path='/tmp/_Neurons.ttl',
+                     out_imports=['ttl/NIF-Phenotype-Core.ttl'],
+                     force_remote = False):
 
-tg = makeGraph('NONE', graph=graphBase.in_graph)
-e = tg.get_equiv_inter(NIFCELL_NEURON)  # FIXME do this on demand
-graphBase.existing_ids = e
+            # check for local base
+                # check if on branch, otherwise fail with a warning
+                # if no outgraph is specified write to the file specified in in_graph_path
+            # otherwise pull from remote and save to /tmp/
+
+            remote_core_paths, local_core_paths = self._make_lr(core_graph_paths)
+            remote_in_paths, local_in_paths = self._make_lr(in_graph_paths)
+            remote_out_path, local_out_path = [_[0] for _ in self._make_lr([out_graph_path])]  # XXX fail w/ tmp
+            remote_out_imports, local_out_imports = self._make_lr(out_imports)
+            if force_remote:
+                use_core_paths = remote_core_paths
+                use_in_paths = remote_in_paths
+            elif os.path.exists(self.local_base):
+                repo = Repo(self.local_base)
+                if repo.active_branch.name != self.branch:
+                    raise FileNotFoundError('Local git repo not on %s branch! Please run `git checkout %s` in %s' % (self.branch, self.branch, self.local_base))
+                use_core_paths = local_core_paths
+                use_in_paths = local_in_paths
+            else:
+                print("Warning local ontology path '%s' not found!" % self.local_base)
+                #raise FileNotFoundError('Local ontology path \'%s\' not found!' % self.local_base)
+                use_core_paths = remote_core_paths
+                use_in_paths = remote_in_paths
+                #if not out_graph_path:  # TODO?
+                    #pass
+
+            # core graph setup
+            graphBase.core_graph = Graph()
+            for cg in use_core_paths:
+                graphBase.core_graph.parse(cg, format='turtle')
+
+            graphBase._predicates = getPhenotypePredicates(graphBase.core_graph)
+
+            # input graph setup
+            graphBase.in_graph = graphBase.core_graph
+            for ig in use_in_paths:
+                graphBase.in_graph.parse(ig, format='turtle')
+
+            ig = makeGraph('',  # fast way to attach prefixes
+                           prefixes=makePrefixes('ILXREPLACE', 'GO', 'CHEBI'),
+                           graph=graphBase.in_graph)
+
+            # output graph setup
+            graphBase.out_graph = Graph()
+            self.ng = makeGraph('',
+                                prefixes=makePrefixes('owl',
+                                                      'GO',
+                                                      'PR',
+                                                      'CHEBI',
+                                                      'UBERON',
+                                                      'NCBITaxon',
+                                                      'ILXREPLACE',
+                                                      'ilx',
+                                                      'ILX',
+                                                      'NIFCELL',
+                                                      'NIFMOL',),
+                                graph=graphBase.out_graph)
+
+            self.ng.filename = out_graph_path
+            ontid = URIRef('file://' + out_graph_path)
+            self.ng.add_ont(ontid, 'Some Neurons')
+            for remote_out_import in remote_out_imports:
+                self.ng.add_node(ontid, 'owl:imports', URIRef(remote_out_import))  # core should be in the import closure
+
+            #e = ig.get_equiv_inter(NIFCELL_NEURON)  # FIXME do this on demand
+            #graphBase.existing_ids = e
+
+            g = globals()
+            g['WRITE'] = self.ng.write
+
+            g['pred'] = graphBase._predicates
+
+        def _make_lr(self, suffixes):
+            local = [self.local_base + s for s in suffixes]
+            remote = [self.remote_base + self.branch + '/' + s for s in suffixes]
+            return local, remote
+
+        @property
+        def _predicates(self):
+            return graphBase._predicates
+
+        @property
+        def out_graph_path(self):
+            return self.ng.filename
+
+        @out_graph_path.setter
+        def out_graph_path(self, value):
+            self.ng.filename = value
+
+        def newOutGraph():
+            graphBase.outGraph = Graph()
+            self.ng = makeGraph('', prefixes=makePrefixes(*self.ng.namespaces), graph=graphBase.outGraph)
+
+    def __new__(cls, *args, **kwargs):
+        if Controller.instance is None:
+            Controller.instance = Controller.__Controller(**kwargs)
+        elif kwargs:
+            Controller.instance = Controller.__Controller(**kwargs)
+        return Controller.instance
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
+
+Controller()
+
+pred = Controller()._predicates
 
 def WRITE():
-    newGraph.write()
+    Controller().ng.write()  # singleton works
 
 def WRITEPYTHON(neurons):
     out = '#!/usr/bin/env python3\n'
