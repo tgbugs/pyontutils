d7f42e6cda9e9567ec8db30e5e8116d2446cf00d
Tom Gillespie <tgbugs@gmail.com>
2017-05-10 13:21:45 -0700

    qnamefix updated to be more efficient

--- pyontutils/qnamefix.py
+++ pyontutils/qnamefix.py
@@ -8,7 +8,38 @@ import os
 from glob import glob
 import rdflib
 from pyontutils.utils import makePrefixes, PREFIXES, makeGraph
+from pyontutils.process_fixed import ProcessPoolExecutor
 from IPython import embed
+
+def convert(f):
+    print(f)
+    graph = rdflib.Graph()
+    graph.parse(f, format='turtle')
+    namespaces = [str(n) for p, n in graph.namespaces()]
+    prefs = []
+    asdf = {v:k for k, v in ps.items()}
+    asdf.update(pi)
+    for rn, rp in asdf.items():
+        for uri in list(graph.subjects()) + list(graph.predicates()) + list(graph.objects()):
+            if type(uri) == rdflib.BNode:
+                continue
+            if uri.startswith(rn):
+                #if rp == 'OBO' or rp == 'IAO' or rp == 'NIFTTL':
+                if rp == 'IAO' or rp == 'NIFTTL':  # XXX this is ok AFTER 0f1f95c25
+                    if rp == 'IAO' and 'IAO_0000412' in uri:
+                        pass
+                    else:
+                        continue
+                prefs.append(rp)
+                break
+
+    if prefs:
+        ps = makePrefixes(*prefs)
+    else:
+        ps = makePrefixes('rdfs')
+    ng = makeGraph(os.path.splitext(f)[0], prefixes=ps, writeloc=os.path.expanduser('~/git/NIF-Ontology/ttl/'))
+    [ng.add_node(*n) for n in graph.triples([None]*3)]
+    ng.write()
 
 def main():
     PREFIXES.pop('OIO')  # prefer oboInOwl
@@ -18,34 +49,8 @@ def main():
     PREFIXES.update(ps)
     pi = {v:k for k, v in PREFIXES.items()}
     pi.pop(None)
-    missing = []
-    for f in glob('*/*.ttl') + glob('*.ttl'):
-        graph = rdflib.Graph()
-        graph.parse(f, format='turtle')
-        namespaces = [str(n) for p, n in graph.namespaces()]
-        prefs = []
-        asdf = {v:k for k, v in ps.items()}
-        asdf.update(pi)
-        for rn, rp in asdf.items():
-            for uri in list(graph.subjects()) + list(graph.predicates()) + list(graph.objects()):
-                if type(uri) == rdflib.BNode:
-                    continue
-                if uri.startswith(rn):
-                    if rp == 'OBO' or rp == 'IAO' or rp == 'NIFTTL':
-                        if rp == 'IAO' and 'IAO_0000412' in uri:
-                            pass
-                        else:
-                            continue
-                    prefs.append(rp)
-                    break
-
-        if prefs:
-            ps = makePrefixes(*prefs)
-        else:
-            ps = makePrefixes('rdfs')
-        ng = makeGraph(os.path.splitext(f)[0], prefixes=ps, writeloc=os.path.expanduser('~/git/NIF-Ontology/ttl/'))
-        [ng.add_node(*n) for n in graph.triples([None]*3)]
-        ng.write()
+    with ProcessPoolExecutor(4) as ppe:
+        futures = [ppe.submit(convert, f) for f in glob('*/*.ttl') + glob('*.ttl')]
 
 if __name__ == '__main__':
     main()
