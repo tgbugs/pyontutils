6b785ecd89c0f522947ae2c53f2765e1de6f4815
Tom Gillespie <tgbugs@gmail.com>
2017-05-10 13:57:35 -0700

    qnamefix dealt with OBO in a more consistent way

--- pyontutils/qnamefix.py
+++ pyontutils/qnamefix.py
@@ -11,22 +11,33 @@ from pyontutils.utils import makePrefixes, PREFIXES, makeGraph
 from pyontutils.process_fixed import ProcessPoolExecutor
 from IPython import embed
 
+PREFIXES.pop('OIO')  # prefer oboInOwl
+
 def convert(f):
-    print(f)
+    ps = {'NIFFIXME':'http://ontology.neuinfo.org/NIF/#',
+          'PROTEGE':'http://protege.stanford.edu/plugins/owl/protege#',
+          'DCFIXME':'http://purl.org/dc/terms/', }
+    PREFIXES.update(ps)
+    pi = {v:k for k, v in PREFIXES.items()}
+    pi.pop(None)
+
     graph = rdflib.Graph()
     graph.parse(f, format='turtle')
     namespaces = [str(n) for p, n in graph.namespaces()]
     prefs = []
     asdf = {v:k for k, v in ps.items()}
     asdf.update(pi)
+
+    # determine which prefixes we need
     for rn, rp in asdf.items():
         for uri in list(graph.subjects()) + list(graph.predicates()) + list(graph.objects()):
             if type(uri) == rdflib.BNode:
                 continue
-            if uri.startswith(rn):
-                #if rp == 'OBO' or rp == 'IAO' or rp == 'NIFTTL':
-                if rp == 'IAO' or rp == 'NIFTTL':  # XXX this is ok AFTER 0f1f95c25
+            elif uri.startswith(rn):
+                if rp == 'OBO' or rp == 'IAO' or rp == 'NIFTTL':
                     if rp == 'IAO' and 'IAO_0000412' in uri:
+                        pass
+                    elif f == 'NIF-Dysfunction.ttl':  # only case where OBO is needed by itself
                         pass
                     else:
                         continue
@@ -39,16 +50,10 @@ def convert(f):
         ps = makePrefixes('rdfs')
     ng = makeGraph(os.path.splitext(f)[0], prefixes=ps, writeloc=os.path.expanduser('~/git/NIF-Ontology/ttl/'))
     [ng.add_node(*n) for n in graph.triples([None]*3)]
+    print(f, len(ng.g))
     ng.write()
 
 def main():
-    PREFIXES.pop('OIO')  # prefer oboInOwl
-    ps = {'NIFFIXME':'http://ontology.neuinfo.org/NIF/#',
-          'PROTEGE':'http://protege.stanford.edu/plugins/owl/protege#',
-          'DCFIXME':'http://purl.org/dc/terms/', }
-    PREFIXES.update(ps)
-    pi = {v:k for k, v in PREFIXES.items()}
-    pi.pop(None)
     with ProcessPoolExecutor(4) as ppe:
         futures = [ppe.submit(convert, f) for f in glob('*/*.ttl') + glob('*.ttl')]
 
