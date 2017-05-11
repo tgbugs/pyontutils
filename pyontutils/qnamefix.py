a25b121f7c5fa1b95a3894d3f5a4e481755c20cd
Tom Gillespie <tgbugs@gmail.com>
2017-05-10 18:25:13 -0700

    qnamefix specific handlers for parcellation schemes

--- pyontutils/qnamefix.py
+++ pyontutils/qnamefix.py
@@ -14,9 +14,7 @@ from IPython import embed
 PREFIXES.pop('OIO')  # prefer oboInOwl
 
 def convert(f):
-    ps = {'NIFFIXME':'http://ontology.neuinfo.org/NIF/#',
-          'PROTEGE':'http://protege.stanford.edu/plugins/owl/protege#',
-          'DCFIXME':'http://purl.org/dc/terms/', }
+    ps = {'PROTEGE':'http://protege.stanford.edu/plugins/owl/protege#', }
     PREFIXES.update(ps)
     pi = {v:k for k, v in PREFIXES.items()}
     pi.pop(None)
@@ -25,6 +23,11 @@ def convert(f):
     graph.parse(f, format='turtle')
     namespaces = [str(n) for p, n in graph.namespaces()]
     prefs = []
+
+    if f == 'NIF-Dysfunction.ttl':
+        prefs.append('OBO')
+
+
     asdf = {v:k for k, v in ps.items()}
     asdf.update(pi)
 
@@ -35,12 +38,7 @@ def convert(f):
                 continue
             elif uri.startswith(rn):
                 if rp == 'OBO' or rp == 'IAO' or rp == 'NIFTTL':
-                    if rp == 'IAO' and 'IAO_0000412' in uri:
-                        pass
-                    elif rp == 'OBO' and f == 'NIF-Dysfunction.ttl':  # only case where OBO is needed by itself
-                        pass
-                    else:
-                        continue
+                    continue
                 prefs.append(rp)
                 break
 
@@ -48,14 +46,27 @@ def convert(f):
         ps = makePrefixes(*prefs)
     else:
         ps = makePrefixes('rdfs')
+
+    if 'parcellation/' in f:
+        nsl = {p:n for p, n in graph.namespaces()}
+        if '' in nsl:
+            ps[''] = nsl['']
+        elif 'hbaslim' in f:
+            ps['HBA'] = nsl['HBA']
+        elif 'mbaslim' in f:
+            ps['MBA'] = nsl['MBA']
+        elif 'cocomac' in f:
+            ps['cocomac'] = nsl['cocomac']
+
     ng = makeGraph(os.path.splitext(f)[0], prefixes=ps, writeloc=os.path.expanduser('~/git/NIF-Ontology/ttl/'))
     [ng.add_node(*n) for n in graph.triples([None]*3)]
-    print(f, len(ng.g))
+    #print(f, len(ng.g))
     ng.write()
 
 def main():
-    with ProcessPoolExecutor(4) as ppe:
-        futures = [ppe.submit(convert, f) for f in glob('*/*.ttl') + glob('*.ttl')]
+    with ProcessPoolExecutor(8) as ppe:
+        futures = [ppe.submit(convert, f) for f in glob('*/*/*.ttl') + glob('*/*.ttl') + glob('*.ttl')]
+        #futures = [ppe.submit(convert, f) for f in glob('generated/parcellation/*.ttl')]
 
 if __name__ == '__main__':
     main()
