20792f482231829e5074a8aa37ddcc9937920e35
Tom Gillespie <tgbugs@gmail.com>
2017-07-28 16:37:36 -0700

    nif_load added code to pull out all classes with some aux information for Burak

--- pyontutils/nif_load.py
+++ pyontutils/nif_load.py
@@ -2,6 +2,8 @@
 """ Run in NIF-Ontology/ttl/ """
 
 import os
+import json
+import yaml
 from glob import glob
 import rdflib
 from pyontutils.utils import makeGraph, makePrefixes  # TODO make prefixes needs an all...
@@ -37,7 +39,56 @@ with open(os.path.expanduser('~/git/NIF-Ontology/scigraph/nifstd_curie_map.yaml'
     wat = yaml.load(f)
 vals = set(wat.values())
 
-mg = makeGraph('nifall', makePrefixes('NIFTTL', 'owl', 'skos'), graph=graph)
+mg = makeGraph('nifall', makePrefixes('NIFTTL', 'owl', 'skos', 'oboInOwl'), graph=graph)
+mg.del_namespace('')
+
+old_namespaces = list(graph.namespaces())
+# may have to copy to a new graph instead
+#[mg.add_namespace(n, p) for n, p in old_namespaces if n.startswith('ns') or n.startswith('default')]
+#[mg.del_namespace(n) for n in list(mg.namespaces)]
+#graph.namespace_manager.reset()
+#[mg.add_namespace(n, p) for n, p in wat.items() if n != '']
+
+def for_burak():
+    syn_predicates = (mg.expand('OBOANN:synonym'),
+                      mg.expand('OBOANN:acronym'),
+                      mg.expand('OBOANN:abbrev'),
+                      mg.expand('oboInOwl:hasExactSynonym'),
+                      mg.expand('oboInOwl:hasNarrowSynonym'),
+                      mg.expand('oboInOwl:hasBroadSynonym'),
+                      mg.expand('oboInOwl:hasRelatedSynonym'),
+                      mg.expand('skos:prefLabel'),
+                      rdflib.URIRef('http://purl.obolibrary.org/obo/go#systematic_synonym'),
+                     )
+    lab_predicates = rdflib.RDFS.label,
+    for s in graph.subjects(rdflib.RDF.type, rdflib.OWL.Class):
+        if not isinstance(s, rdflib.BNode):
+            try:
+                prefix, namespace, name = graph.namespace_manager.compute_qname(s, False)
+                curie = ':'.join((prefix, name))
+            except (KeyError, ValueError) as e:
+                curie = str(s)
+            labels = list(o for p in lab_predicates for o in graph.objects(s, p)
+                          if not isinstance(o, rdflib.BNode))
+            synonyms = list(o for p in syn_predicates for o in graph.objects(s, p)
+                            if not isinstance(o, rdflib.BNode))
+            parents = []
+            for o in graph.objects(s, rdflib.RDFS.subClassOf):
+                if not isinstance(o, rdflib.BNode):
+                    try:
+                        prefix, namespace, name = graph.namespace_manager.compute_qname(o, False)
+                        o = ':'.join((prefix, name))
+                    except (KeyError, ValueError) as e:
+                        o = str(o)
+                    parents.append(o)
+            yield [curie, labels, synonyms, parents]
+
+#globals()['for_burak'] = for_burak
+#embed()
+records = {c:[l, s, p] for c, l, s, p in for_burak() if l or s}
+with open(os.path.expanduser('~/files/ontology-classes-with-labels-synonyms-parents.json'), 'wt') as f:
+          json.dump(records, f, sort_keys=True, indent=2)
+
 j = mg.make_scigraph_json('owl:imports', direct=True)
 #asdf = sorted(set(_ for t in graph for _ in t if type(_) == rdflib.URIRef))  # this snags a bunch of other URIs
 #asdf = sorted(set(_ for _ in graph.subjects() if type(_) != rdflib.BNode))
