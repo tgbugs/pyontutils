6597f80149d0b469ae8de99e6f802fb73f84dadd
Tom Gillespie <tgbugs@gmail.com>
2017-09-05 15:39:57 -0700

    ttlcmp bugfixes

--- pyontutils/ttlcmp.sh
+++ pyontutils/ttlcmp.sh
@@ -7,7 +7,9 @@ TTLCMP=$ONT/ttlcmp
 cd $TTL ;
 ontload imports NIF-Ontology NIF *.ttl */*.ttl */*/*.ttl -l $GIT &&
 java -cp $GIT/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App $TTL/*.ttl $TTL/*/*.ttl $TTL/*/*/*.ttl &&
-find $TTL -type d -exec mkdir -p $TTLCMP/{} \;
-ttlfmt $ONT/ttlcmp/*.ttl $ONT/ttlcmp/*/*.ttl $ONT/ttlcmp/*/*/*.ttl &&
-cd $TTL ;
+find -type d -exec mkdir -p ../ttlcmp/{} \;
+find -name '*.ttl.ttl' -exec bash -c 'mv $1 ../ttlcmp/$1' _ {} \;
+ttlfmt $ONT/ttlcmp/*.ttl &&
+ttlfmt $ONT/ttlcmp/*/*.ttl &&
+ttlfmt $ONT/ttlcmp/*/*/*.ttl &&
 find -name '*.ttl' -exec bash -c 'diff -u $1 ../ttlcmp/$1.ttl >> /tmp/ttlcmp.patch' _ {} \;
