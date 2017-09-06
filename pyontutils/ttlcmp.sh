1984b8b08b7df81719fe75e93c4d3e4b12e8041b
Tom Gillespie <tgbugs@gmail.com>
2017-09-06 03:56:09 -0700

    ttlcmp.sh igmore version line for diffs and injected : prefix (for now)

--- pyontutils/ttlcmp.sh
+++ pyontutils/ttlcmp.sh
@@ -12,5 +12,5 @@ find -name '*.ttl.ttl' -exec bash -c 'mv $1 ../ttlcmp/$1' _ {} \;
 ttlfmt $ONT/ttlcmp/*.ttl &&
 ttlfmt $ONT/ttlcmp/*/*.ttl &&
 ttlfmt $ONT/ttlcmp/*/*/*.ttl &&
-find -name '*.ttl' -exec bash -c 'diff -u $1 ../ttlcmp/$1.ttl >> /tmp/ttlcmp.patch' _ {} \;
+find -name '*.ttl' -exec bash -c 'diff -u <(head -n -1 $1) <(head -n -1 ../ttlcmp/$1.ttl | tail -n +2) >> /tmp/ttlcmp.patch' _ {} \;
 git checkout *.ttl */*.ttl */*/*.ttl
