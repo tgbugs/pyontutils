36b52bdc44d70793e8ed552aa0efa08dd54c0830
Tom Gillespie <tgbugs@gmail.com>
2017-09-12 03:49:47 -0700

    make_catalog.py updated so that all files are added and any local copies of external imports also have their canonical iri mapped

--- pyontutils/make_catalog.py
+++ pyontutils/make_catalog.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python3
 import os
-import glob
+from glob import glob
+from pyontutils.ontload import local_imports
 
-
-urlbase = 'http://ontology.neuinfo.org/NIF/ttl/'
+remote_base = 'http://ontology.neuinfo.org/NIF/ttl/'
 base = '~/git/NIF-Ontology/ttl/'
+
 #list of all nif ontologies
-fs = glob.glob(os.path.expanduser(base) + '*')
-fs += glob.glob(os.path.expanduser(base) + '*/*')
+local_base = os.path.expanduser(base)
+b = local_base
+fs = glob(b + '*')
+fs += glob(b + '*/*')
+fs += glob(b + '*/*/*')
+onts = [f for f in fs if f.endswith('.ttl') or f.endswith('.owl')]
 
 _ = [print(f) for f in fs]
 
-mapping = [(urlbase + file.split('/ttl/', 1)[-1], file.split('/ttl/', 1)[-1]) for file in fs if file.endswith('.ttl') or file.endswith('.owl')]
+mapping = [(remote_base + file.split('/ttl/', 1)[-1], file.split('/ttl/', 1)[-1])
+           for file in fs
+           if file.endswith('.ttl')
+           or file.endswith('.owl')]
+
+# check for mismatched import and ontology iris
+itrips = local_imports(remote_base, local_base, onts, readonly=True, dobig=True)
+sa = {os.path.basename(o):s for s, p, o in itrips if 'sameAs' in p}
+for f in fs:
+    if '/external/' in f:
+        basename = os.path.basename(f)
+        if basename in sa:
+            target = sa[basename]
+            if 'external' not in target:
+                mapping.append((target, 'external/' + basename))
+
+print(mapping)
 
 # make a protege catalog file to simplify life
 uriline = '  <uri id="User Entered Import Resolution" name="{ontid}" uri="{filename}"/>'
 
 xmllines = ['<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
 '<catalog prefer="public" xmlns="urn:oasis:names:tc:entity:xmlns:xml:catalog">',] + \
-[uriline.format(ontid=ont, filename=file) for ont,file in mapping] + \
-['  <group id="Folder Repository, directory=, recursive=true, Auto-Update=true, version=2" prefer="public" xml:base=""/>',
-'</catalog>',]
+[uriline.format(ontid=ont, filename=file) for ont,file in sorted(mapping)] + \
+['</catalog>']
 xml = '\n'.join(xmllines)
 with open('/tmp/nif-catalog-v001.xml','wt') as f:
     f.write(xml)
-
