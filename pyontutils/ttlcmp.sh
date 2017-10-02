dc38c5709a02c70d67b174ea487d67b3278044fa
Tom Gillespie <tgbugs@gmail.com>
2017-10-02 02:37:07 -0700

    ttlcmp ordering fixes and don't fail if missing some converted files

--- pyontutils/ttlcmp.sh
+++ pyontutils/ttlcmp.sh
@@ -5,11 +5,11 @@ TTL=$ONT/ttl
 TTLCMP=$ONT/ttlcmp
 
 cd $TTL ;
+find -type d -exec mkdir -p $TTLCMP/{} \;
 ontload imports NIF-Ontology NIF {*,*/*,*/*/*}.ttl -l $GIT &&
-java -cp $GIT/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App $TTL/{*,*/*,*/*/*}.ttl &&
-find -type d -exec mkdir -p ../ttlcmp/{} \;
+java -cp $GIT/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App $TTL/{*,*/*,*/*/*}.ttl ;
 find -name '*.ttl.ttl' -exec bash -c 'mv $1 ../ttlcmp/$1' _ {} \;
 ttlfmt $ONT/ttlcmp/{*,*/*,*/*/*}.ttl &&
 echo '# comparing current serialization to files serialized from full imports' > /tmp/ttlcmp.patch ;
-find -name '*.ttl' -exec bash -c 'echo "# "$1 >> /tmp/ttlcmp.patch ; diff -u <(head -n -1 $1) <(head -n -1 ../ttlcmp/$1.ttl) >> /tmp/ttlcmp.patch' _ {} \;
+find $TTLCMP -name '*.ttl.ttl' -exec bash -c 'echo "# "$0 >> /tmp/ttlcmp.patch ; diff -u <(head -n -1 $(echo $0 | rev | cut -d"." -f2- | rev | sed "s/cmp\//\//" | tee -a /tmp/ttlcmp.log)) <(head -n -1 $0) >> /tmp/ttlcmp.patch' {} \;
 git checkout {*,*/*,*/*/*}.ttl
