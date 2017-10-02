b62e7e19747ff04394467f8e62d893180e6425db
Tom Gillespie <tgbugs@gmail.com>
2017-10-02 05:21:47 -0700

    make_catalog.py better handling of external ontologies

--- pyontutils/make_catalog.py
+++ pyontutils/make_catalog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 from glob import glob
+from pyontutils.utils import anyMembers
 from pyontutils.ontload import local_imports
 
 remote_base = 'http://ontology.neuinfo.org/NIF/ttl/'
@@ -12,7 +13,7 @@ b = local_base
 fs = glob(b + '*')
 fs += glob(b + '*/*')
 fs += glob(b + '*/*/*')
-onts = [f for f in fs if f.endswith('.ttl') or f.endswith('.owl')]
+onts = [f for f in fs if f.endswith('.ttl') or f.endswith('.owl') and 'NEMO_' not in f]
 
 _ = [print(f) for f in fs]
 
@@ -24,8 +25,13 @@ mapping = [(remote_base + file.split('/ttl/', 1)[-1], file.split('/ttl/', 1)[-1]
 # check for mismatched import and ontology iris
 itrips = local_imports(remote_base, local_base, onts, readonly=True, dobig=True)
 sa = {os.path.basename(o):s for s, p, o in itrips if 'sameAs' in p}
+
+# FIXME should be able to do this by checking what is tracked by git...
+externals = ('CogPO.owl', 'NEMO_vv2.98.owl', 'cogat_v0.3.owl', 'doid.owl',
+             'ero.owl', 'pato.owl', 'pr.owl', 'ro_bfo1-1_bridge.owl', 'uberon.owl')
+
 for f in fs:
-    if '/external/' in f:
+    if '/external/' in f and anyMembers(f, *externals):
         basename = os.path.basename(f)
         if basename in sa:
             target = sa[basename]
