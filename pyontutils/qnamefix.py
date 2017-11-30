43e4fc10947c65c6217cc6cec18639c9213f854f
Tom Gillespie <tgbugs@gmail.com>
2017-11-29 18:41:24 -0800

    qnamefix.py extracted the file loading from the graph so that it can be reused

--- pyontutils/qnamefix.py
+++ pyontutils/qnamefix.py
@@ -21,21 +21,55 @@ from docopt import docopt
 from pyontutils.utils import makePrefixes, PREFIXES, makeGraph
 
 exclude = 'generated/swanson_hierarchies.ttl', 'generated/NIF-NIFSTD-mapping.ttl'
+PREFIXES.pop('NIFTTL')
+
 
 def convert(f, prefixes):
     if f in exclude:
         print('skipping', f)
         return f
-    pi = {v:k for k, v in prefixes.items()}
 
     graph = rdflib.Graph()
     graph.parse(f, format='turtle')
+
+    def prefix_cleanup(ps, graph):
+        if 'parcellation/' in f:
+            nsl = {p:n for p, n in graph.namespaces()}
+            if '' in nsl:
+                ps[''] = nsl['']
+            elif 'hbaslim' in f:
+                ps['HBA'] = nsl['HBA']
+            elif 'mbaslim' in f:
+                ps['MBA'] = nsl['MBA']
+            elif 'cocomac' in f:
+                ps['cocomac'] = nsl['cocomac']
+
+        # special cases for NIFORG, NIFINV, NIFRET where there identifiers in
+        # annotation properties that share the prefix, so we warn incase
+        # at some point in the future for some reason want them again...
+        if f == 'NIF-Organism.ttl':
+            print('WARNING: special case for NIFORG')
+            ps.pop('NIFORG')
+        elif f == 'NIF-Investigation.ttl':
+            print('WARNING: special case for NIFINV')
+            ps.pop('NIFINV')
+        elif f == 'unused/NIF-Retired.ttl':
+            print('WARNING: special case for NIFRET')
+            ps.pop('NIFGA')
+
+    ng = cull_prefixes(graph, cleanup=prefix_cleanup)
+    ng.filename = f
+    #[ng.add_trip(*n) for n in graph.triples([None]*3)]
+    #print(f, len(ng.g))
+    ng.write()
+    return f
+
+def cull_prefixes(graph, prefixes=PREFIXES, cleanup=lambda ps, graph: None):
     namespaces = [str(n) for p, n in graph.namespaces()]
     prefs = ['']
-
+    pi = {v:k for k, v in prefixes.items()}
     asdf = {} #{v:k for k, v in ps.items()}
     asdf.update(pi)
-
     # determine which prefixes we need
     for uri in list(graph.subjects()) + list(graph.predicates()) + list(graph.objects()):
         if uri.endswith('.owl') or uri.endswith('.ttl'):
@@ -53,42 +87,15 @@ def convert(f, prefixes):
     else:
         ps = makePrefixes('rdfs')
 
-    if 'parcellation/' in f:
-        nsl = {p:n for p, n in graph.namespaces()}
-        if '' in nsl:
-            ps[''] = nsl['']
-        elif 'hbaslim' in f:
-            ps['HBA'] = nsl['HBA']
-        elif 'mbaslim' in f:
-            ps['MBA'] = nsl['MBA']
-        elif 'cocomac' in f:
-            ps['cocomac'] = nsl['cocomac']
-
-    # special cases for NIFORG, NIFINV, NIFRET where there identifiers in
-    # annotation properties that share the prefix, so we warn incase
-    # at some point in the future for some reason want them again...
-    if f == 'NIF-Organism.ttl':
-        print('WARNING: special case for NIFORG')
-        ps.pop('NIFORG')
-    elif f == 'NIF-Investigation.ttl':
-        print('WARNING: special case for NIFINV')
-        ps.pop('NIFINV')
-    elif f == 'unused/NIF-Retired.ttl':
-        print('WARNING: special case for NIFRET')
-        ps.pop('NIFGA')
+    cleanup(ps, graph)
 
     ng = makeGraph('', prefixes=ps)
-    ng.filename = f
     [ng.g.add(t) for t in graph]
-    #[ng.add_trip(*n) for n in graph.triples([None]*3)]
-    #print(f, len(ng.g))
-    ng.write()
-    return f
+    return ng
 
 def main():
     from joblib import Parallel, delayed
     args = docopt(__doc__, version = "qnamefix 0")
-    PREFIXES.pop('NIFTTL')
     for x in args['--exclude']:
         PREFIXES.pop(x)
     if args['--slow'] or len(args['<file>']) == 1:
