3acbe86d47d138a7d1a7e3fcda7f8b74b44a75ea
Tom Gillespie <tgbugs@gmail.com>
2017-12-04 04:33:34 -0800

    tests and supporting files added for scottl, note that scogood.ttl is not actually correct right now

--- test/test_ttlser.py
+++ test/test_ttlser.py
@@ -1,6 +1,6 @@
 import inspect
 import os
-import random
+from random import shuffle
 import rdflib
 import re
 import subprocess
@@ -8,11 +8,30 @@ import sys
 import unittest
 
 rdflib.plugin.register('nifttl', rdflib.serializer.Serializer, 'pyontutils.ttlser', 'CustomTurtleSerializer')
+rdflib.plugin.register('scottl', rdflib.serializer.Serializer, 'pyontutils.ttlser', 'SubClassOfTurtleSerializer')
+
+
+def randomize_prefix_order(graph):
+    namespace = graph.namespace_manager.store._IOMemory__namespace
+    prefix = graph.namespace_manager.store._IOMemory__prefix
+    def save(d): return {k:v for l in (list(d.items()),)
+                         for k, v in l
+                         if not shuffle(l)}
+    sn, sp = save(namespace), save(prefix)
+    def zap(d): [d.pop(k) for k in list(d.keys())]
+    zap(namespace)
+    zap(prefix)
+    graph.namespace_manager.reset()
+    def readd(s, d):
+        for k, v in s.items():
+            d[k] = v
+    readd(sn, namespace)
+    readd(sp, prefix)
 
 def randomize_BNode_order(graph):
     replaced = {}
     urn = [f'{i:0<6}' for i in range(999999)]
-    random.shuffle(urn)
+    shuffle(urn)
     def swap(t):
         if isinstance(t, rdflib.BNode):
             if t in replaced:
@@ -30,53 +49,70 @@ def randomize_BNode_order(graph):
             graph.add(new_trip)
 
 class TestTtlser(unittest.TestCase):
+
+    goodpath = 'test/good.ttl'
+    scogoodpath = 'test/scogood.ttl'
+    badpath = 'test/nasty.ttl'
+    actualpath = 'test/actual.ttl'
+    actualpath2 = 'test/actual2.ttl'
+    scoactualpath = 'test/scoactual.ttl'
+    scoactualpath2 = 'test/scoactual2.ttl'
+
     def setUp(self):
-
-        goodpath = 'test/good.ttl'
-        self.badpath = 'test/nasty.ttl'
-        self.actualpath = 'test/actual.ttl'
-        self.actualpath2 = 'test/actual2.ttl'
-
-        with open(goodpath, 'rb') as f:
+        with open(self.goodpath, 'rb') as f:
             self.good = f.read()
+        with open(self.scogoodpath, 'rb') as f:
+            self.scogood = f.read()
 
         self.actual = self.serialize()
         with open(self.actualpath, 'wb') as f:
             f.write(self.actual)
+
+        self.scoactual = self.serialize(outfmt='scottl')
+        with open(self.actualpath, 'wb') as f:
+            f.write(self.scoactual)
         
-    def make_ser(self):
+    def make_ser(self, outfmt='nifttl'):
         header = ('import sys\n'
-                  'import random\n'
+                  'from random import shuffle\n'
                   'import rdflib\n'
                   "rdflib.plugin.register('nifttl', rdflib.serializer.Serializer, 'pyontutils.ttlser', 'CustomTurtleSerializer')\n"
                   'class Thing:\n'
                   '    badpath = \'%s\'\n') % self.badpath
         src0 = inspect.getsource(self.serialize)
         src1 = inspect.getsource(randomize_BNode_order)
-        after =  't = Thing()\nsys.stdout.buffer.write(t.serialize())\n'
-        return header + src0 + '\n\n' + src1 + after
+        src2 = inspect.getsource(randomize_prefix_order)
+        after =  f't = Thing()\nsys.stdout.buffer.write(t.serialize(\'{outfmt}\'))\n'
+        return header + src0 + '\n\n' + src1 + '\n\n' + after
 
-    def serialize(self):
+    def serialize(self, outfmt='nifttl'):
         graph = rdflib.Graph()
         graph.parse(self.badpath, format='turtle')
         randomize_BNode_order(graph)
-        actual = graph.serialize(format='nifttl')
+        randomize_prefix_order(graph)
+
+        actual = graph.serialize(format=outfmt)
         actual = actual.rsplit(b'\n',2)[0]  # drop versioninfo
         return actual
 
-    def test_ser(self):
-        assert self.actual == self.good
-
-    def test_deterministic(self):
+    def deterministic(self, outfmt='nifttl'):
         nofail = True
         env = os.environ.copy()
         seed = None  # 'random'
+        if outfmt == 'nifttl':
+            actual = self.actual
+            actualpath = self.actualpath
+            actualpath2 = self.actualpath2
+        elif outfmt == 'scottl':
+            actual = self.scoactual
+            actualpath = self.scoactualpath
+            actualpath2 = self.scoactualpath2
         for _ in range(20):
             if seed is not None:
                 env['PYTHONHASHSEED'] = str(seed)
             else:
                 env.pop('PYTHONHASHSEED', None)
-            cmd_line = [sys.executable, '-c', self.make_ser()]
+            cmd_line = [sys.executable, '-c', self.make_ser(outfmt)]
             p = subprocess.Popen(cmd_line, stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, #stderr=subprocess.STDOUT,
                                  env=env)
@@ -84,7 +120,7 @@ class TestTtlser(unittest.TestCase):
             out = re.sub(br"\[\d+ refs, \d+ blocks\]\r?\n?", b"", out)  # nose can't import strip_python_stderr from any test submodule :/
             #out = out.split(b'\n', 1)[1]  # don't need to remove the rdflib noise if using >=rdflib-5.0.0
             actual2 = out
-            if self.actual != actual2:
+            if actual != actual2:
                 print('Determinism failure!')
                 if False:
                     hit = False
@@ -94,11 +130,23 @@ class TestTtlser(unittest.TestCase):
                         if hit:
                             print(_1, _2)
                 nofail = False
-                with open(self.actualpath2, 'wb') as f:
+                with open(actualpath2, 'wb') as f:
                     f.write(actual2)
-                with open(self.actualpath, 'wb') as f:
-                    f.write(self.actual)
+                with open(actualpath, 'wb') as f:
+                    f.write(actual)
                 break
 
-        assert nofail
+        return nofail
+
+    def test_ser(self):
+        assert self.actual == self.good
+
+    def test_scoser(self):
+        assert self.scoactual == self.scogood
+
+    def test_deterministic(self):
+        assert self.deterministic()
+
+    def test_scodet(self):
+        assert self.deterministic('scottl')
 
