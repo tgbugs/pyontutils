7455970b86f77f1c82be3a434e742b4f0fd602ff
Tom Gillespie <tgbugs@gmail.com>
2018-01-30 13:22:05 -0800

    ontree impl local, main page, fixed massive bugs in getArgs

--- pyontutils/ontree.py
+++ pyontutils/ontree.py
@@ -3,9 +3,10 @@
 
 Usage:
     ontree [options] <predicate-curie> <root-curie>
-    ontree server
+    ontree server [options]
 
 Options:
+    -k --key=APIKEY         apikey for SciGraph instance [default: None]
     -f --input-file=FILE    don't use SciGraph, load an individual file instead
     -o --outgoing           if not specified defaults to incoming
     -b --both               if specified goes in both directions
@@ -18,7 +19,7 @@ from datetime import datetime
 from inspect import getsourcelines
 from urllib.error import HTTPError
 import rdflib
-from docopt import docopt
+from docopt import docopt, parse_defaults
 from flask import Flask, url_for, redirect, request, render_template, render_template_string, make_response, abort 
 from pyontutils.hierarchies import Query, creatTree, dematerialize
 from pyontutils.scigraph_client import Graph
@@ -46,8 +47,8 @@ def graphFromGithub(link):
     print(link)
     return makeGraph('', graph=rdflib.Graph().parse(f'{link}?raw=true', format='turtle'))
 
-def render(pred, root, direction=None, depth=10, local_filepath=None, branch='master', restriction=False, wgb='FIXME'):
-    kwargs = {}
+def render(pred, root, direction=None, depth=10, local_filepath=None, branch='master', restriction=False, wgb='FIXME', local=False):
+    kwargs = {'local':local}
     prov = [
             f'<title>Transitive closure of {root} under {pred}</title>'
             f'<meta name="date" content="{datetime.utcnow().isoformat()}">',
@@ -84,16 +85,33 @@ def render(pred, root, direction=None, depth=10, local_filepath=None, branch='ma
         return abort(422, message)
 
 def getArgs(request):
-    want = {'direction':inc,
+    want = {'direction':inc,  # INCOMING OUTGOING BOTH
             'depth':10,
             'branch':'master',
-            'restriction':False,
+            'restriction':False,  # True False
+            'local':False,  # True False  # canonoical vs scigraph ? interlex?
            }
-    return {w:request.args.get((True if w.lower() == 'true' else
-                                (False if w.lower() == 'false' else w)), v)
-            for w, v in want.items()}
 
-def server():
+    def convert(k):
+        if k in request.args:
+            v = request.args[k]
+        else:
+            return want[k]
+
+        if isinstance(want[k], bool):
+            if v.lower() == 'true':
+                return True
+            elif v.lower() == 'false':
+                return False
+            else:
+                raise TypeError(f'Expected a bool, got "{v}" instead.')
+        else:
+            return v
+
+    return {k:convert(k)
+            for k, v in want.items()}
+
+def server(api_key=None):
     f = os.path.realpath(__file__)
     __file__name = os.path.basename(f)
     __file__path = os.path.dirname(f)
@@ -110,10 +128,21 @@ def server():
     app = Flask('ontology tree service')
     basename = 'trees'
 
+    def link(href, value=None):
+        if value is None:
+            value = href
+        return f'<a href="{href}">{value}</a>'
+
     @app.route(f'/{basename}', methods=['GET'])
     @app.route(f'/{basename}/', methods=['GET'])
     def route_():
-        return 'TODO'
+        d = url_for('route_docs')
+        e = url_for('route_examples')
+        return ('<html><body>'
+                f'{link(d, "Docs")}'
+                '<br>'
+                f'{link(e, "Examples")}'
+                '</body><html>')
 
     @app.route(f'/{basename}/docs', methods=['GET'])
     def route_docs():
@@ -155,7 +184,7 @@ def server():
         return ('<html>'
                 '<body>'
                 '<table><tr><th align="left">Root class</th><th align="left">'
-                '../query/{predicate-curie}/{root-curie}?direction=INCOMING&depth=10&branch=master</th></tr>'
+                '../query/{predicate-curie}/{root-curie}?direction=INCOMING&depth=10&branch=master&local=false</th></tr>'
                 f'{links}</table>'
                 '<table><tr><th align="left">Root class</th><th align="left">'
                 '../query/{predicate-curie}/{root-curie}/{ontology-filepath}?direction=INCOMING&depth=10&branch=master&restriction=false</th></tr>'
@@ -181,6 +210,8 @@ def server():
         except HTTPError:
             return abort(404, 'Unknown ontology file.')  # TODO 'Unknown git branch.'
 
+    if api_key:
+        sgg.api_key = api_key
     app.debug = False
     app.run(host='localhost', port=8000, threaded=True)  # nginxwoo
     # FIXME pypy3 has some serious issues yielding when threaded=True, gil issues?
@@ -189,7 +220,11 @@ def server():
 def main():
     from docopt import docopt
     args = docopt(__doc__, version='ontree 0.0.0')
+    defaults = {o.name:o.value if o.argcount else None for o in parse_defaults(__doc__)}
+
     if args['server']:
+        k = args['--key']
+        k = k if k != defaults['--key'] else None
         server()
     else:
         direction = both if args['--both'] else out if args['--incoming'] else inc
