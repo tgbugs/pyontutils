dda9c7ce9ff13830121b184535fa7c045eb04d1b
tmsincomb <troysincomb@gmail.com>
2018-05-16 17:12:34 -0700

    doctest added

--- ilxutils/ilxutils/mydifflib.py
+++ ilxutils/ilxutils/mydifflib.py
@@ -4,8 +4,8 @@ from pyontutils.utils import TermColors
 
 
 
-''' --word-diff=porcelain clone'''
 def diff(s1, s2):
+    ''' --word-diff=porcelain clone'''
     delta = difflib.Differ().compare(s1.split(), s2.split())
     difflist = []
     fullline = ''
@@ -23,8 +23,8 @@ def diff(s1, s2):
         difflist.append(fullline[:-1])
     return [l[:] for l in '\n'.join(difflist).splitlines() if l]
 
-''' --word-diff=color clone '''
 def diffcolor(s1, s2):
+    ''' --word-diff=color clone '''
     string = ''
     for line in diff(s1, s2):
         if line[0] == '-':
@@ -35,31 +35,33 @@ def diffcolor(s1, s2):
             string += ' ' + line
     return string[1:]
 
-''' ratio of likeness btw 2 strings '''
 def ratio(s1, s2):
+    ''' ratio of likeness btw 2 strings '''
     return difflib.SequenceMatcher(None, s1, s2).ratio()
 
-''' creates basic html based on the diff of 2 strings '''
 def create_html(s1, s2, output):
+    ''' creates basic html based on the diff of 2 strings '''
     html = difflib.HtmlDiff().make_file(s1.split(), s2.split())
     with open(output, 'w') as f:
         f.write(html)
 
-def main():
-    s1 = "the neuron's went up the tall stairs."
-    s2 = "the neurons went up the tall stairs."
-    s1 = open('../dum1.txt', 'r').read().strip()
-    s2 = open('../dum2.txt', 'r').read().strip()
-    dif = diff(s1, s2)
-    json.dump({'id':1234, 'term':'neuron', 'difflib':dif, 'ratio':ratio(s1, s2)}, open('mydifftest.json', 'w'), indent=4)
-    for i,l in enumerate(dif):
-        if l[0] == '-':
-            print(TermColors.red(l))
-        elif l[0] == '+':
-            print(TermColors.green(l))
-        else:
-            print(l)
-    print(diffcolor(s1, s2))
+def test():
+    """ checking output for diff and colordiff
+
+    >>> s1 = "the neuron's went up the tall stairs."
+    >>> s2 = "the neurons went up the tall stairs again."
+
+    >>> diff(s1, s2)
+    ['the', "- neuron's", '+ neurons', 'went up the tall', '- stairs.', '+ stairs', '+ again.']
+
+    #script is right but output has color sep. from string.
+    #>>> diffcolor(s1, s2)
+    #"the \x1b[91mneuron's\x1b[0m \x1b[32mneurons\x1b[0m went up the tall \x1b[91mstairs.\x1b[0m \x1b[32mstairs\x1b[0m \x1b[32magain.\x1b[0m"
+
+    >>> ratio(s1, s2)
+    0.9113924050632911
+    """
 
 if __name__ == '__main__':
-    main()
+    import doctest
+    doctest.testmod()
