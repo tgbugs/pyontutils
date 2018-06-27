7e1867146a6d6ca15f968de9a793c80d1dba4b72
Tom Gillespie <tgbugs@gmail.com>
2018-06-27 16:10:29 -0700

    neuron example temporary fix for leaking local names and context, should finally fix broken tests

--- pyontutils/neuron_models/ma2015.py
+++ pyontutils/neuron_models/ma2015.py
@@ -6,11 +6,7 @@ from pyontutils.neuron_lang import *
 from pyontutils.phenotype_namespaces import BBP
 from IPython import embed
 
-if __name__ == '__main__':
-    # FIXME proper cleanup in testing not this
-    # breaks testing by preventing return to master
-    Config('markram-2015')
-    #config(out_graph_path=Path(graphBase.local_base, 'ttl/hbp-special.ttl').as_posix())
+Config('markram-2015')
 
 with BBP:
     context = Neuron(Rat, S1, INT, GABA)
