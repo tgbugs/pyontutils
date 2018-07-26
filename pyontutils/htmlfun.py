32d38ee88801ef2242d230fe305224c027a5feb6
Tom Gillespie <tgbugs@gmail.com>
2018-07-25 18:36:40 -0700

    htmlfun added navbar + style and divtag

--- pyontutils/htmlfun.py
+++ pyontutils/htmlfun.py
@@ -7,8 +7,8 @@ def tag(_tag, n=False):
     nl = '\n' if n else ''
     s = f'<{_tag}>{nl}'
     e = f'{nl}</{_tag}>'
-    def tagwrap(value):
-        return s + value + e
+    def tagwrap(*value):
+        return s + nl.join(value) + e
     return tagwrap
 
 def atag(href, value=None, new_tab=False, uriconv=None):
@@ -18,6 +18,11 @@ def atag(href, value=None, new_tab=False, uriconv=None):
         if uriconv is not None:
             href = uriconv(href)
     return f'<a href="{href}"{target}>{value}</a>'
+
+def divtag(*values, cls=None):
+    class_ = f'class="{cls}"' if cls else ''
+    vals = '\n'.join(values)
+    return f'<div {class_}>\n{vals}\n</div>'
 
 def deltag(text):
     return f'<del>{text}</del>'
@@ -29,13 +34,14 @@ styletag = tag('style', n=True)
 scripttag = tag('script', n=True)
 bodytag = tag('body', n=True)
 
-def htmldoc(body, title='Spooky Nameless Page', styles=tuple(), scripts=tuple()):
+from IPython import embed
+def htmldoc(*body, title='Spooky Nameless Page', styles=tuple(), scripts=tuple()):
     header = ('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"\n'
               '"http://www.w3.org/TR/html4/loose.dtd">\n')
     styles = '\n'.join((styletag(s) for s in styles))
     scripts = '\n'.join((scripts(s) for s in scripts))
     head = headtag('\n'.join((titletag(title), '<meta charset="UTF-8">', styles, scripts)))
-    return header + htmltag('\n'.join((head, bodytag(body))))
+    return header + htmltag('\n'.join((head, bodytag(*body))))
 
 def render_table(rows, *headers):
     output = []
@@ -61,4 +67,36 @@ table_style = ('th { text-align: left; padding-right: 20px; }'
 details_style = ('details summary::-webkit-details-marker { display: none; }\n'
                  'details > summary:first-of-type { list-style-type: none; }')
 
+navbar_style = '''
+.navbar {
+    overflow: hidden;
+    background-color: gray;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+}
+
+.navbar a {
+    float: left;
+    display: block;
+    color: black;
+    font-family: Dejavu Sans Mono;
+    font-weight: bold;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change background on mouse-over */
+.navbar a:hover {
+    background: #ddd;
+    color: black;
+}
+
+/* Main content */
+.main {
+    margin-top: 60px;
+}
+'''
 
