e220978c0e820f0bd9ca2c25d6fe8cc79bcd4ccd
Tom Gillespie <tgbugs@gmail.com>
2018-07-25 23:15:14 -0700

    neuron_models __path__ fails in nosetests so use __file__ \!??\!

--- pyontutils/neuron_models/__init__.py
+++ pyontutils/neuron_models/__init__.py
@@ -1,3 +1,6 @@
 import pkgutil
+from pathlib import Path
 
-__all__ = [mod for _, mod, is_pkg in pkgutil.walk_packages(__path__)]
+# __path__ fails in nosetests for reasons I do not understand
+thispath = [Path(__file__).parent.as_posix()]
+__all__ = [mod for _, mod, is_pkg in pkgutil.walk_packages(thispath)]
