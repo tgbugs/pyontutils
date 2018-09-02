e767234018d799106837bcbe9bf81e27414dafc0
Tom Gillespie <tgbugs@gmail.com>
2018-09-02 16:46:31 -0700

    ttlcmp.sh reorganization and speedup using xargs

--- pyontutils/ttlcmp.sh
+++ pyontutils/ttlcmp.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
-GIT=${HOME}/git
-ONT=${HOME}/git/NIF-Ontology
+# run this not on your main copy of the repo in case something goes wrong
+GIT=/tmp
+ONT=$GIT/NIF-Ontology
 TTL=$ONT/ttl
 TTLCMP=$ONT/ttlcmp
 
 cd $TTL ;
+FILES=$(git ls-files | grep ".ttl")
 find -type d -exec mkdir -p $TTLCMP/{} \;
-ontload imports NIF-Ontology NIF {*,*/*,*/*/*}.ttl -l $GIT &&
-java -cp $GIT/ttl-convert/target/ttl-convert-1.0-SNAPSHOT-jar-with-dependencies.jar scicrunch.App $TTL/{*,*/*,*/*/*}.ttl ;
+ontload imports NIF-Ontology NIF $FILES -l $GIT &&
+
+for f in $FILES;
+do echo -e ${f};
+done | xargs -L 1 -P 8 ttl-convert &&
+
 find -name '*.ttl.ttl' -exec bash -c 'mv $1 ../ttlcmp/$1' _ {} \;
 ttlfmt $ONT/ttlcmp/{*,*/*,*/*/*}.ttl &&
 echo '# comparing current serialization to files serialized from full imports' > /tmp/ttlcmp.patch ;
 find $TTLCMP -name '*.ttl.ttl' -exec bash -c 'echo "# "$0 >> /tmp/ttlcmp.patch ; diff -u <(head -n -1 $(echo $0 | rev | cut -d"." -f2- | rev | sed "s/cmp\//\//" | tee -a /tmp/ttlcmp.log)) <(head -n -1 $0) >> /tmp/ttlcmp.patch' {} \;
-git checkout {*,*/*,*/*/*}.ttl
+git checkout $FILES
