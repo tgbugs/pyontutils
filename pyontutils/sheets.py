50a82708992193d8cbe21d090bcdd776fbef6394
Tom Gillespie <tgbugs@gmail.com>
2018-10-09 02:00:00 -0700

    first pass at sheets api to pull down neuron data

---
+++ pyontutils/sheets.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.6
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from pyontutils.config import devconfig
+from IPython import embed
+
+# the first time you run this you will need to use the --noauth_local_webserver args
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+def get_oauth_service():
+    store = file.Storage('google-api-token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('google-api-creds.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    return service
+
+def get_sheet_values(spreadsheet_name, sheet_name):
+    SPREADSHEET_ID = devconfig.secrets(spreadsheet_name)
+    service = get_oauth_service()
+    ss = service.spreadsheets()
+    res = ss.get(spreadsheetId=SPREADSHEET_ID, includeGridData=True).execute()
+    # res['sheets'][1]['data'][0]['rowData'][0]  # lota data but no comments yet ...
+    #embed()
+    result = ss.values().get(spreadsheetId=SPREADSHEET_ID, range=sheet_name).execute()
+    values = result.get('values', [])
+    return values
+
+def main():
+    values = get_sheet_values('neurons-cut', 'CUT V1.0')
+    embed()
+
+if __name__ == '__main__':
+    main()
