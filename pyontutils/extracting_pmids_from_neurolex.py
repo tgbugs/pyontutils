c6e68d89b0cb11c0bdb880dcd763917089e0882e
tmsincomb <troysincomb@gmail.com>
2018-10-10 13:37:04 -0700

    added file extracting_pmids_from_neurolex.py to do just that in interlex

---
+++ pyontutils/extracting_pmids_from_neurolex.py
@@ -0,0 +1,196 @@
+from collections import defaultdict
+import csv
+from pathlib import Path
+import os
+from rdflib import Graph, URIRef, Literal, BNode, RDF, RDFS, OWL
+from sqlalchemy import create_engine, inspect, Table, Column
+from sqlalchemy.orm.session import sessionmaker
+from typing import Dict, Tuple, List, Union
+
+
+db_url = os.environ.get('SCICRUNCH_DB_URL_PRODUCTION')
+ilx_uri_base = 'http://uri.interlex.org/base'
+triple2annotation_bnode = {}
+g = Graph()
+output = '/home/tmsincomb/Desktop/anno-test2.ttl'
+
+
+with open('/home/tmsincomb/Dropbox/PMID/pmid-dump.tsv', 'r') as csvFile:
+    reader = csv.reader(csvFile, delimiter='\t')
+    old_text = []
+    for i, row in enumerate(reader):
+        if i == 0:
+            header = {colname: col_indx for col_indx, colname in enumerate(row)}
+            continue
+        old_text.append(row[header['old_text']])
+    csvFile.close()
+
+
+namespaces = {
+    'ILX': 'http://uri.interlex.org/base/ilx_',
+    'definition': 'http://purl.obolibrary.org/obo/IAO_0000115',
+    'ilxtr': 'http://uri.interlex.org/tgbugs/uris/readable/',
+    'owl': 'http://www.w3.org/2002/07/owl#',
+}
+for prefix, uri in namespaces.items():
+    g.bind(prefix, uri)
+
+
+def add_annotation(
+        subj: URIRef,
+        pred: URIRef,
+        obj: Union[Literal, URIRef],
+        a_p: URIRef ,
+        a_o: Union[Literal, URIRef],
+    ) -> BNode:
+    """ Adds annotation to rdflib graph.
+
+    The annotation axiom will filled in if this is a new annotation for the triple.
+
+    Args:
+        subj: Entity subject to be annotated
+        pref: Entities Predicate Anchor to be annotated
+        obj: Entities Object Anchor to be annotated
+        a_p: Annotation predicate
+        a_o: Annotation object
+
+    Returns:
+        A BNode which is an address to the location in the RDF graph that is storing the
+        annotation information.
+    """
+    bnode: BNode = triple2annotation_bnode.get( (subj, pred, obj) )
+    if not bnode:
+        a_s: BNode = BNode()
+        triple2annotation_bnode[ (subj, pred, obj) ]: BNode = a_s
+        g.add((a_s, RDF.type, OWL.Axiom))
+        g.add((a_s, OWL.annotatedSource, subj))
+        g.add((a_s, OWL.annotatedProperty, pred))
+        g.add((a_s, OWL.annotatedTarget, obj))
+    else:
+        a_s: BNode = bnode
+    g.add( (a_s, a_p, a_o) )
+    return bnode # In case you have more triples to add
+
+
+def get_existing_ids():
+    Session = sessionmaker()
+    engine = create_engine(db_url)
+    Session.configure(bind=engine)
+    session = Session()
+    sql = f"""
+        SELECT
+            tei.id, tei.tid, tei.curie, tei.iri, tei.preferred,
+            t.ilx, t.type, t.label, t.definition, t.comment
+        FROM (
+            SELECT *
+            FROM terms
+            GROUP BY terms.ilx
+        ) as t
+        JOIN term_existing_ids AS tei
+        ON t.id = tei.tid
+    """ # groub by will just take the first occurance and drop the rest
+    sqlobj = session.execute(sql)
+    return sqlobj
+
+
+def get_suffix2row():
+    suffix2row = {}
+    for row in get_existing_ids():
+         suffix2row[row.iri.rsplit('/', 1)[-1]] = {
+            'ilx': row.ilx, 'curie': row.curie, 'iri': row.iri, 'definition':row.definition,
+        }
+    return suffix2row
+
+
+def pmid_fix(string):
+    def uni_strip(s):
+        return s.replace('PMID=', '').replace('PMID:', '').replace('.', '').strip()
+    if ',' in string:
+        obj = [uni_strip(s) for s in string.split(',')]
+    elif ';' in string:
+        obj = [uni_strip(s) for s in string.split(';')]
+    else:
+        obj = [uni_strip(string)]
+    new_obj = []
+    for o in obj:
+        if 'PMID' in o:
+            new_obj.append(o.split('PMID')[-1].strip())
+        else:
+            new_obj.append(o)
+    obj = new_obj
+    new_obj = []
+    for o in obj:
+        try:
+            if 'pmc' not in o.lower():
+                int(o)
+            new_obj.append(o)
+        except:
+            count = 0
+            for e in o:
+                try:
+                    int(e)
+                    count += 1
+                except:
+                    pass
+            if count > 0:
+                print(o) #p357, D011919 D-- is a literal ID of the mesh annotation
+    return new_obj
+
+
+def main():
+    data = [text for text in old_text
+        if 'SuperCategory=Resource' not in text and 'Id=\\n' not in text and 'PMID=\\n' not in text]
+
+    ### PRIMER
+    id_count, pmid_count = 0, 0
+    total_data = {}
+    for d in data:
+        local_data = {'id':None, 'pmids':set()}
+        for segment in d.split('|'):
+            if 'Id=' == segment[:3]:
+                id_count += 1
+                local_data['id'] = segment
+            if 'PMID=' == segment[:5]:
+                pmid_count += 1
+                local_data['pmids'].add(segment)
+        if local_data['id'] and local_data['pmids']:
+            total_data[local_data['id']] = local_data['pmids']
+    print(id_count, pmid_count)
+
+    ### Nuances
+    raw_id2pmids = total_data
+    total_data = defaultdict(list)
+    for exid, pmids in raw_id2pmids.items():
+        for hit in exid.split('\\n'):
+            if 'Id=' in hit:
+                clean_id = hit.replace('Id=', '').strip()
+        clean_pmids = []
+        for hit in [pmid.split('\\n') for pmid in pmids]:
+            for h in hit:
+                if 'PMID=' in h:
+                    h = pmid_fix(h)
+                    clean_pmids.extend(h)
+        if clean_pmids:
+            total_data[clean_id].extend(clean_pmids)
+    suffix2pmids = total_data
+
+    ### Annotation Creation
+    suffix2row = get_suffix2row()
+    for _id, pmids in suffix2pmids.items():
+        row = suffix2row.get(_id)
+        if row:
+            for pmid in pmids:
+                ilx_uri = URIRef('/'.join([ilx_uri_base, row['ilx']]))
+                add_annotation(
+                    ilx_uri,
+                    URIRef('http://purl.obolibrary.org/obo/IAO_0000115'),
+                    Literal(row['definition']),
+                    URIRef('http://uri.interlex.org/tgbugs/uris/readable/literatureCitation'),
+                    Literal('PMID:'+pmid)
+                )
+
+    g.serialize(output, format='turtle')
+
+
+if __name__ == '__main__':
+    main()
