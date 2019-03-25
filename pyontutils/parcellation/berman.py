5e58f1d6ccc775549e7c63d42f012ca0c33f6291
Tom Gillespie <tgbugs@gmail.com>
2019-03-25 19:50:25 -0400

    parcellation berman many updates and corrections

--- pyontutils/parcellation/berman.py
+++ pyontutils/parcellation/berman.py
@@ -1,49 +1,144 @@
+import subprocess
 import collections
-from PIL import Image, ImageEnhance
-from pytesseract import image_to_string, image_to_data
 from pathlib import Path
-from glob import glob
-from sys import exit
 import json
+from pyontutils.config import devconfig
 
-SRC = Path.home()/'Dropbox/misc-work/brainmap/cropped/'
-DESTINATION = str(Path.home()/'Dropbox/misc-work/brainmap/brainmap_legends.json')
+source_images = Path('~/files/cropped').expanduser()
+output = Path(devconfig.resources, 'berman_legends.json')
+
 
 def clean(string):
     ''' Begining of the string can sometimes have odd noise '''
-    return string.replace('_', '').replace('-', '').replace('—', '').replace('.', '').replace('=', '').strip()
+    # manual fixes in the source
+    # 24/1.png
+    # 9/1.png
+    # 3/1.png
+    #if ')' in string:  # fix in the source data
+        #string = string.split(')')[0] + ')'  # remove trailing garbage
+    return (string
+            .replace('_', '')
+            .replace('-', '')
+            .replace('—', '')
+            .replace('.', '')
+            .replace('=', '')
+            .replace('\u2018',"'")  # LEFT SINGLE QUOTATION MARK
+            .replace('\u2019', "'")  # RIGHT SINGLE QUOTATION MARK
+            .strip())
+
 
 def get_legends(raw_text):
     legends = []
     for line in raw_text.splitlines():
+        line = clean(line)
         if not line:
             continue
-        line = clean(line)
         try:
             abbrev, label = line.split(' ', 1)
-        except:
-            print(line)
-            print(raw_text)
-            exit()
+        except ValueError as e:
+            print(repr(line))
+            print(repr(raw_text))
+            raise e
+            continue
         abbrev = clean(abbrev)
         label = clean(label)
         legends.append((abbrev, label))
     return legends
 
+
+redo = False
 data = []
-for foldername in glob(str(SRC/'*')):
-    img_num = int(Path(foldername).stem)
-    print('num', img_num)
-    legends = []
-    for img in glob(str(SRC/foldername/'*')):
-        img = Image.open(img)
-        raw_text = image_to_string(img, lang='eng', config='--psm 6') # '--psm 4 also works but 6 seems to work better'
-        legends += get_legends(raw_text)
+for folder in source_images.glob('*'):
+    raw = folder / 'raw.txt'
+    img_num = int(folder.stem)
+    if not raw.exists() or redo:
+        legends = []
+        raw_text = ''
+        for img in folder.glob('*.png'):
+            print('num', img_num, img.stem)
+            p = subprocess.Popen(('tesseract', img.as_posix(), 'stdout', '-l', 'eng', '--oem', '2', '--psm', '6'),
+                                 stdout=subprocess.PIPE)
+            bytes_text, err = p.communicate()
+            raw_text += bytes_text.decode() + '\n'
+
+        with open(raw, 'wt') as f:
+            f.write(raw_text)
+    else:
+        with open(raw, 'rt') as f:
+            raw_text = f.read()
+
+    legends = get_legends(raw_text)
     data.append((img_num, legends))
 
-final = collections.OrderedDict()
-for key, value in sorted(data, key=lambda d: d[0]):
-    final[key] = value
 
-with open(DESTINATION, 'w') as outfile:
-    json.dump(final, outfile, indent=4)
+# in theory could use the most frequent if > .75 are the same ...
+cor_l = {
+    'abducens nerve': {'GN': '6N'},
+    'alaminar spinal trigeminal nucleus, magnocellular division (14)': {'5SM': 'SSM'},
+    'alaminar spinal trigeminal nucleus, parvocellular division (6)': {'5SP': 'SSP'},
+    'central nucleus of the inferior colliculus (21)': {'1CC': 'ICC'},
+    'cerebral cortex': {'¢': 'C'},
+    'commissure of the inferior colliculi': {'1CO': 'ICO', 'I1CO': 'ICO'},
+    'corpus callosum': {'198': 'CC'},
+    'inferior central nucleus (13)': {'C': 'CI', 'Cl': 'CI'},
+    'lateral tegmental field (3)': {'FIL': 'FTL'},
+    'mesencephalic trigeminal nucleus (19)': {'SME': '5ME'},
+    'motor trigeminal tract': {'SMT': '5MT'},
+    'nucleus of the trapezoid body (15)': {'J': 'T'},
+    'solitary tract': {'$': 'S'},
+    'spinal trigeminal tract': {'SST': '5ST'},
+    'statoacoustic nerve': {'BN': 'SN'},
+    'superior central nucleus (22)': {'s': 'CS'},
+    'trigeminal nerve': {'SN': '5N'},
+}
+
+cor_a = {
+    #'1': {'ependymal layer', 'superficial layer'},
+    #'2': {'intermediate layer', 'molecular layer'},
+    #'3': {'deep layer', 'oculomotor nucleus (27)', 'pyramidal layer'},
+    #'4': {'polymorph layer', 'trochlear nucleus (23)'},
+    'KF': {'KollikerFuse nucleus (17)': 'KéllikerFuse nucleus (17)'},
+    'SCS': {'superior colliculus, supertficial layer (25)':
+            'superior colliculus, superficial layer (25)'},
+}
+
+
+by_abbrev = collections.defaultdict(list)
+by_label = collections.defaultdict(list)
+abbrev_index = collections.defaultdict(list)
+label_index = collections.defaultdict(list)
+for n, legends in sorted(data):
+    for abbrev, label in legends:
+        if label in cor_l and abbrev in cor_l[label]:
+            abbrev = cor_l[label][abbrev]
+        if abbrev in cor_a and label in cor_a[abbrev]:
+            label = cor_a[abbrev][label]
+        by_abbrev[abbrev].append(label)
+        by_label[label].append(abbrev)
+        abbrev_index[abbrev].append(n)
+        label_index[label].append(n)
+
+
+def dorder(thing, type=lambda v:v):
+    return  {k:type(v) for k, v in sorted(thing.items())}
+
+by_abbrev = dorder(by_abbrev, collections.Counter)
+by_label = dorder(by_label, collections.Counter)
+index_abbrev = dorder(abbrev_index)
+index_label = dorder(label_index)
+
+prob_a = {k:v for k, v in by_abbrev.items() if len(v) > 1}
+prob_l = {k:v for k, v in by_label.items() if len(v) > 1}
+
+
+from IPython import embed
+embed()
+
+
+def out():
+    final = collections.OrderedDict()
+    for key, value in sorted(data, key=lambda d: d[0]):
+        final[key] = value
+
+    with open(output, 'wt') as f:
+        json.dump(final, f, indent=2)
