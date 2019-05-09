34022f4e54230411e1d4697322eff9d21a8c7e49
tmsincomb <troysincomb@gmail.com>
2019-05-09 14:42:31 -0700

    moved google sheets and fixed time()

--- nifstd/nifstd_tools/ontree.py
+++ nifstd/nifstd_tools/ontree.py
@@ -46,7 +46,6 @@ from IPython import embed
 import yaml
 
 log = makeSimpleLogger('ontree')
-time = str(datetime.utcnow().isoformat()).replace('.', ',')
 
 sgg = scigraph.Graph(cache=False, verbose=True)
 sgv = scigraph.Vocabulary(cache=False, verbose=True)
@@ -68,6 +67,11 @@ both = 'BOTH'
 UBERON_TERMS = 'uberon-terms'
 SPINAL_TERMINOLOGY = 'spinal-terminology'
 PARCELLATION_BRAINSTEM = 'parcellation-brainstem'
+
+
+def time():
+    return str(datetime.utcnow().isoformat()).replace('.', ',')
+
 
 def convert_view_text_to_dict() -> dict:
     with open(Path(devconfig.resources, 'sparc_terms_populated.txt'), 'rt') as infile:
@@ -104,6 +108,7 @@ def normt(term, prefix=''):
         row = [prefix + term] + curies
     return row
 
+
 def tag_curies(curies):
     tagged_curies = []
     for curie in curies:
@@ -112,6 +117,7 @@ def tag_curies(curies):
         tagged_curies.append(tagged_curie)
     return tagged_curies
 
+
 def linearize_graph(dict_: dict, tier_level: int = 0) -> tuple:
     """ Recursively pull nested dictionaries out of print order"""
     for key, value in dict_.items():
@@ -119,192 +125,6 @@ def linearize_graph(dict_: dict, tier_level: int = 0) -> tuple:
         yield (label, curies, tier_level)
         if isinstance(value, dict):
             yield from linearize_graph(value, tier_level + 1)
-
-
-# class UberonTerms(Sheet):
-#     name = UBERON_TERMS
-#     sheet_name = 'Sheet1'
-#
-#     def get_terms(self):
-#         ''' Uberon has trailing empty cells in the sheet assuming its the value of the most recent
-#             left cell with a value '''
-#         terms_list = []
-#         terms_index = 1
-#         sub_terms_index = 2
-#         last_value_index = 0
-#         terms_dict = defaultdict(list)
-#         records = []
-#         for i, term in enumerate(self.raw_values[terms_index]):
-#             sub_term = self.values[sub_terms_index][i]
-#             if term:
-#                 records.append(
-#                     (self.name, term, i)
-#                 )
-#                 last_term = term
-#             if sub_term:
-#                 records.append(
-#                     (self.name, '|_____ '+sub_term, i)
-#                 )
-#         return records
-#
-#     def get_term_list(self, colummn):
-#         start_index = 3
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class SpinalTerminology1(Sheet):
-#     name = SPINAL_TERMINOLOGY
-#     sheet_name = 'Sheet1'
-#
-#     def get_terms(self):
-#         terms_list = []
-#         headers_index = 0
-#         headers_column_start = 1 # 1 header doesnt have a value
-#         for i, term in enumerate(self.raw_values[headers_index][headers_column_start:], headers_column_start):
-#             terms_list.append((SPINAL_TERMINOLOGY_1, term, i))
-#         return terms_list
-#
-#     def get_term_list(self, colummn):
-#         start_index = 1
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class SpinalTerminology2(Sheet):
-#     name = SPINAL_TERMINOLOGY
-#     sheet_name = 'Sheet2'
-#
-#     def get_terms(self):
-#         terms_list = []
-#         headers_index = 0
-#         headers_column_start = 0
-#         for i, term in enumerate(self.raw_values[headers_index][headers_column_start:]):
-#             terms_list.append((SPINAL_TERMINOLOGY_1, term, i))
-#         return terms_list
-#
-#     def get_term_list(self, colummn):
-#         start_index = 1
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class ParcellationBrainstemMappings(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'Mappings'
-#
-#     def get_terms(self):
-#         terms_list = []
-#         headers_index = 0
-#         headers_column_start = 0
-#         for i, term in enumerate(self.raw_values[headers_index][headers_column_start:]):
-#             term = f'{term} (Mappings)'
-#             terms_list.append((PARCELLATION_BRAINSTEM_MAPPINGS, term, i))
-#         return terms_list
-#
-#     def get_term_list(self, colummn):
-#         start_index = 1
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class ParcellationBrainstemUberon(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'UBERON'
-#
-#     def get_terms(self):
-#         terms_list = []
-#         headers_index = 0
-#         headers_column_start = 0
-#         for i, term in enumerate(self.raw_values[headers_index][headers_column_start:]):
-#             terms_list.append((PARCELLATION_BRAINSTEM_UBERON, term, i))
-#         return terms_list
-#
-#     def get_term_list(self, colummn):
-#         start_index = 1
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class ParcellationBrainstemAllenMouse(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'Allen Mouse'
-#
-#     def get_terms(self):
-#         return [(PARCELLATION_BRAINSTEM_ALLEN_MOUSE, 'Allen Mouse Sheet', 0)]
-#
-#     def get_term_list(self, colummn):
-#         start_index = 0
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class ParcellationBrainstemPaxinosRat(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'Paxinos Rat'
-#
-#     def get_terms(self):
-#         return [(PARCELLATION_BRAINSTEM_PAXINOS_RAT, 'Paxinos Rat Sheet', 0)]
-#
-#     def get_term_list(self, colummn):
-#         start_index = 0
-#         return [row[1:3] for row in self.values[start_index:]]
-#
-#
-# class ParcellationBrainstemBermanCat(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'Berman Cat'
-#
-#     def get_terms(self):
-#         return [(PARCELLATION_BRAINSTEM_BERMAN_CAT, 'Berman Cat Sheet', 0)]
-#
-#     def get_term_list(self, colummn):
-#         start_index = 0
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-#
-#
-# class ParcellationBrainstemNieuwenhuys(Sheet):
-#     name = PARCELLATION_BRAINSTEM
-#     sheet_name = 'Nieuwenhuys'
-#
-#     def get_terms(self):
-#         terms_list = []
-#         headers_index = 0
-#         headers_column_start = 0
-#         for i, term in enumerate(self.raw_values[headers_index][headers_column_start:]):
-#             terms_list.append((PARCELLATION_BRAINSTEM_NIEUWENHUYS, term, i))
-#         return terms_list
-#
-#     def get_term_list(self, colummn):
-#         start_index = 1
-#         return [row[int(colummn)] for row in self.values[start_index:] if row[int(colummn)]]
-
-
-class GoogleSheets:
-
-    def __init__(self):
-        self.sheets = {}
-        self.sheets[UBERON_TERMS] = UberonTerms()
-        self.sheets[SPINAL_TERMINOLOGY_1] = SpinalTerminology1()
-        self.sheets[SPINAL_TERMINOLOGY_2] = SpinalTerminology2()
-        # Mappings not needed at the moment
-        # self.sheets[PARCELLATION_BRAINSTEM_MAPPINGS] = ParcellationBrainstemMappings()
-        self.sheets[PARCELLATION_BRAINSTEM_UBERON] = ParcellationBrainstemUberon()
-        self.sheets[PARCELLATION_BRAINSTEM_ALLEN_MOUSE] = ParcellationBrainstemAllenMouse()
-        self.sheets[PARCELLATION_BRAINSTEM_PAXINOS_RAT] = ParcellationBrainstemPaxinosRat()
-        self.sheets[PARCELLATION_BRAINSTEM_BERMAN_CAT] = ParcellationBrainstemBermanCat()
-        self.sheets[PARCELLATION_BRAINSTEM_NIEUWENHUYS] = ParcellationBrainstemNieuwenhuys()
-
-    def get_terms(self):
-        terms = []
-        terms += self.sheets[UBERON_TERMS].get_terms()
-        terms += self.sheets[SPINAL_TERMINOLOGY_1].get_terms()
-        terms += self.sheets[SPINAL_TERMINOLOGY_2].get_terms()
-        # Mappings not needed at the moment
-        # terms += self.sheets[PARCELLATION_BRAINSTEM_MAPPINGS].get_terms()
-        terms += self.sheets[PARCELLATION_BRAINSTEM_UBERON].get_terms()
-        terms += self.sheets[PARCELLATION_BRAINSTEM_ALLEN_MOUSE].get_terms()
-        terms += self.sheets[PARCELLATION_BRAINSTEM_PAXINOS_RAT].get_terms()
-        terms += self.sheets[PARCELLATION_BRAINSTEM_BERMAN_CAT].get_terms()
-        terms += self.sheets[PARCELLATION_BRAINSTEM_NIEUWENHUYS].get_terms()
-        return terms
-
-    def get_term_list(self, source, column):
-        return self.sheets[source].get_term_list(column)
 
 
 class ImportChain:  # TODO abstract this a bit to support other onts, move back to pyontutils
@@ -710,7 +530,7 @@ def server(api_key=None, verbose=False):
         return htmldoc(
             render_table(rows),
             title = 'Terms for ' + (tier2 if tier2 is not None else tier1),
-            meta=f'<meta name="date" content="{time}">',
+            metas = ({'name':'date', 'content':time()},),
         )
 
     @app.route(f'/{basename}/sparc/view', methods=['GET'])
@@ -734,7 +554,7 @@ def server(api_key=None, verbose=False):
             render_table(hyp_rows),
             title = 'Main Page Sparc',
             styles = ["p {margin: 0px; padding: 0px;}"],
-            meta = f'<meta name="date" content="{time}">',
+            metas = ({'name':'date', 'content':time()},),
         )
 
     @app.route(f'/{basename}/sparc/index', methods=['GET'])
@@ -743,7 +563,7 @@ def server(api_key=None, verbose=False):
         return htmldoc(
             render_table(view_rows),
             title = 'SPARC Anatomical terms index',
-            meta = f'<meta name="date" content="{time}">',
+            metas = ({'name':'date', 'content':time()},),
         )
 
     @app.route(f'/{basename}/sparc', methods=['GET'])
@@ -753,7 +573,7 @@ def server(api_key=None, verbose=False):
             atag(url_for('route_sparc_view'), 'Terms by region or atlas'), '<br>',
             atag(url_for('route_sparc_index'), 'Index'),
             title='SPARC Anatomical terms', styles=["p {margin: 0px; padding: 0px;}"],
-            meta = f'<meta name="date" content="{time}">',
+            metas = ({'name':'date', 'content':time()},),
         )
 
 
