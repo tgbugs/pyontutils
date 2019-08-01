0d5d27f23b895959c781e7c27abf672af4a20a4b
tmsincomb <troysincomb@gmail.com>
2019-08-01 16:55:18 -0700

    Create remotes.py
    easy interlex remote import

---
+++ ilxutils/ilxutils/remotes.py
@@ -0,0 +1,20 @@
+import ontquery as oq
+from pyontutils.core import OntTerm
+import os
+
+InterLexRemote = oq.plugin.get('InterLex')
+
+TEST = 'https://test.scicrunch.org/api/1/'
+PRODUCTION = 'https://scicrunch.org/api/1/'
+
+interlex_remote_production = InterLexRemote(
+    # When ready, should be changed to 'https://scicrunch.org/api/1/' for production
+    apiEndpoint = PRODUCTION
+)
+interlex_remote_production.setup(instrumented=OntTerm)
+
+interlex_remote_test = InterLexRemote(
+    # When ready, should be changed to 'https://scicrunch.org/api/1/' for production
+    apiEndpoint = TEST
+)
+interlex_remote_test.setup(instrumented=OntTerm)
