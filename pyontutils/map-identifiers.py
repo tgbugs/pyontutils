7ffaca46b9664f64e5212c6170380e068302b24b
Tom Gillespie <tgbugs@gmail.com>
2019-09-16 14:07:36 -0700

    mapTempToIndex simplified and broke up implementation

--- pyontutils/map-identifiers.py
+++ pyontutils/map-identifiers.py
@@ -28,61 +28,93 @@ class GraphToMap(OntGraph):
     def couldMap(self, *temp_namespaces):
         yield from (e for p in temp_namespaces for e in self.uriPrefix(p))
 
-    def doMap(self, index_graph, index_namespace, *temp_namespaces):
+    def add_remove_same(self, add_and_replace_graph, *, new_replaces_old=True):
+        """ compute add, remove, same graphs based on a graph
+            the contains triples of the form `new replaces old`
+            where replaces can be any predicate set new_replaces_old=False
+            if the add_and_replace graph is of the form `old replacedBy new`
+        """
+        if new_replaces_old:
+            replace = {old:new for new, _, old in add_and_replace_graph}
+        else:
+            replace = {old:new for old, _, new in add_and_replace_graph}
+
+        def iri_replace(t):
+            return tuple(replace[e] if e in replace else e for e in t)
+
+        add, rem, same = [self.__class__() for _ in range(3)]
+        for t in self:
+            nt = iri_replace(t)
+            if nt != t:
+                add.add(nt), rem.add(t)
+            else:
+                same.add(t)
+
+        return add, rem, same
+
+    def mapTempToIndex(self, index_graph, index_namespace, *temp_namespaces):
         """ In theory index_graph could be self if the namespace in use
             is only relevant for a single file, otherwise there needs to be
             a single shared reference file that maintains the the index for all
             files
+
+            NOTE: either the current graph or the index_graph needs to have the
+                  curie mapping for the namespaces defined prior to calling this
+                  otherwise you will have to cross populate namespaces again
         """
         if not isinstance(index_graph, self.__class__):
             index_graph = self.__class__(existing=index_graph)
 
-        existing  = list(set(index_graph.uriPrefix(index_namespace)))  # target prefix?
+        existing_indexes  = list(set(index_graph.uriPrefix(index_namespace)))  # target prefix?
         lp = len(index_namespace)
-        suffixes = [int(u[lp:]) for u in existing]
+        suffixes = [int(u[lp:]) for u in existing_indexes]
         start = max(suffixes) + 1 if suffixes else 1
 
-        temps = list(set(self.couldMap(*temp_namespaces)))
+        could_map = list(set(self.couldMap(*temp_namespaces)))
         mapped_triples = [(s,
                            ilxtr.hasTemporaryId,
                            o) for s, o in index_graph[:ilxtr.hasTemporaryId:]]
-        #already_mapped = list(set(e for p in temp_namespaces for e in index_graph.uriPrefix(p)))
-        already_mapped = [o for _, _, o in mapped_triples]
         # FIXME could be mapped into another namespace ? what to do in that case?
-        to_map = [t for t in temps if t not in already_mapped] # FIXME TODO temp ids that have not been mapped
+        already_mapped = [o for _, _, o in mapped_triples]
+        not_mapped = [e for e in could_map if e not in already_mapped] # FIXME TODO temp ids that have not been mapped
 
-        new_mapped = [(index_namespace[str(i + start)],
-                       ilxtr.hasTemporaryId,
-                       temp_id)
-                      for i, temp_id in enumerate(sorted(to_map))]
-
-        replace = {old:new for new, _, old in new_mapped}
-        def rep(t):
-            return [replace[e] if e in replace else e for e in t]
+        # the iri replace operation is common enough that it probably
+        # deserves its own semantics since when combined with a known
+        # input qualifier the output graph is well defined and it provides
+        # much stronger and clearer semantics for what was going on
+        # allowing multiple different predicates is fine since different
+        # processes may have different use cases for the predicate
+        # in the case where the mapping is simply stored and added
+        # as an equivalent class along with hasInterLexId, then the new
+        # mappings are simply add rather than addAndReplaceUsingMapping
+        iri_replace_map = [(index_namespace[str(i + start)],
+                            ilxtr.hasTemporaryId,
+                            temp_id)
+                           for i, temp_id in enumerate(sorted(not_mapped))]
 
         # need to compute the id of the graph/triples opaque data section
-        add_graph = self.__class__()
-        [add_graph.add(t) for t in new_mapped]
+        # this is the pure add graph, in this case it is also used to
+        # compute the replace graph as well
+        add_replace_graph = self.__class__()
+        [add_replace_graph.add(t) for t in iri_replace_map]
 
         index_graph.namespace_manager.populate_from(self)
-        index_graph.addN(add_graph.quads())
-
-        if False: # old way of updating index_graph, but index_graph needs to store the state
-            combined_graph = self.__class__()
-            combined_graph.namespace_manager.populate_from(self, index_graph)
-            #self.namespace_manager.populate(combined_graph)
-            #index_graph.namespace_manager.populate(combined_graph)
-            #ns_prefixes = [str(ns) for ns in (index_namespace, *temp_namespaces)]
-            #curie_prefixes = [combined_graph.qname(ns).rstrip(':') for ns in ns_prefixes]
-            combined_graph.addN(index_graph.quads())
-            combined_graph.addN(add_graph.quads())
-            print(combined_graph.ttl)
+        index_graph.addN(add_replace_graph.quads())
 
         new_self = self.__class__()
         new_self.namespace_manager.populate_from(index_graph)
-        [new_self.add(rep(t)) for t in self]
-        new_self.addN(add_graph.quads())
-        return new_self()
+        add_only_graph, remove_graph, same_graph = self.add_remove_same(add_replace_graph)
+        # the other semantics that could be used here
+        # would be to do an in place modification of self
+        # to remove the remove graph and add the add_only_graph
+
+        # NOTE the BNodes need to retain their identity across the 3 graphs
+        # so that they reassemble correctly
+        new_self.addN(add_replace_graph.quads())
+        new_self.addN(add_only_graph.quads())
+        new_self.addN(same_graph.quads())
+        breakpoint()
+        return new_self
 
 
 def graph_with_temporary_ids_to_graph_with_mapped_ids(input_graph, graph_that_keeps_the_identifiers):
@@ -100,16 +132,17 @@ def main():
     from pathlib import Path
     from pyontutils.config import devconfig
     path = Path(devconfig.ontology_local_repo, 'ttl/generated/neurons/huang-2017.ttl')
-    g = GraphToMap()
+
     index_graph = GraphToMap()
-    index_graph.bind('npokb', npokb)
+
     # testing
+    index_graph.bind('npokb', npokb)
     [index_graph.add((npokb[str(i)], rdf.type, owl.Class)) for i in range(1, 11)]
     [index_graph.add((npokb[str(i)], ilxtr.hasTemporaryId, TEMP[str(i)])) for i in range(1, 11)]
 
-    g.parse(path.as_posix(), format='ttl')
-    mapped_graph = g.doMap(index_graph, npokb, TEMP)
-    #mapped_graph.write(path=path)
+    input_graph = GraphToMap()
+    input_graph.parse(path.as_posix(), format='ttl')
+    output_graph = input_graph.mapTempToIndex(index_graph, npokb, TEMP)
     breakpoint()
 
 if __name__ == '__main__':
