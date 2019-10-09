4cc9552f4b8e6d5cd43d7f96d7f5aeda14d0ddaf
Tom Gillespie <tgbugs@gmail.com>
2019-10-08 22:02:15 -0700

    pyontutils setup.py add augpathlib dep

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     python_requires='>=3.6',
     tests_require=tests_require,
     install_requires=[
+        'augpathlib>0.0.1',
         'appdirs',
         'colorlog',
         'docopt',
