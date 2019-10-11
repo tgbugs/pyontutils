2c9ba628846959a22849eea6ec7c34c44597fd31
Tom Gillespie <tgbugs@gmail.com>
2019-10-10 21:24:03 -0700

    ttlser version bump and fix bug serializing list without outfile
    added test_ttlfmt with case that caused the bug

---
+++ ttlser/test/test_ttlfmt.py
@@ -0,0 +1,25 @@
+import sys
+import unittest
+from ttlser.ttlfmt import main
+
+f1 = 'test/good.ttl', 'test/f1.ttl'
+f2 = 'test/nasty.ttl', 'test/f2.ttl'
+
+class TestTtlfmt(unittest.TestCase):
+    argv = ['ttlfmt', f1[1], f2[1], '--slow']
+    def setUp(self):
+        for source, dest in (f1, f2):
+            with open(source, 'rt') as s, open(dest, 'wt') as d:
+                d.write(s.read())
+
+        self.oldargv = sys.argv
+        sys.argv = self.argv
+
+    def tearDown(self):
+        sys.argv = self.oldargv
+
+    def test_multi_slow(self):
+        try:
+            main()
+        except AttributeError as e:
+            raise AttributeError(f'failed with {self.argv}') from e
--- ttlser/ttlser/ttlfmt.py
+++ ttlser/ttlser/ttlfmt.py
@@ -37,6 +37,7 @@ import rdflib
 from rdflib.plugins.parsers.notation3 import BadSyntax
 
 defaults = {o.name:o.value if o.argcount else None for o in parse_defaults(__doc__)}
+GRAPHCLASS = rdflib.Graph
 
 
 def getVersion():
@@ -64,6 +65,7 @@ def prepare(filepath_or_stream, outpath=None, stream=False):
         if outpath is None:
             outpath = filepath_or_stream
         print(filepath_or_stream)
+
     return dict(source=filepath_or_stream,
                 format_guess=infmt_guess,
                 outpath=outpath)
@@ -73,8 +75,8 @@ formats = ('ttl', 'json-ld', None, 'xml', 'n3', 'nt', 'nquads', 'trix',
            'trig', 'hturtle', 'rdfa', 'mdata', 'rdfa1.0', 'html')
 
 
-def parse(source, format_guess, outpath, graph=None, infmt=None):
-    graph = rdflib.Graph() if graph is None else graph
+def parse(source, format_guess, outpath, graph=None, infmt=None, graph_class=GRAPHCLASS):
+    graph = graph_class() if graph is None else graph
     errors = []
     if infmt:
         format_guess = infmt
@@ -123,11 +125,11 @@ def serialize(graph, outpath, outfmt=defaults['--outfmt'], debug=False, profile=
             f.write(out)
 
 
-def convert(file, outpath=None, stream=False,
+def convert(file_or_list_or_stream, outpath=None, stream=False,
             infmt=None, outfmt=defaults['--outfmt'],
-            debug=False, profile=False):
-    if stream or type(file) == str:
-        file_or_stream = file
+            debug=False, profile=False, graph_class=GRAPHCLASS):
+    if stream or type(file_or_list_or_stream) == str:
+        file_or_stream = file_or_list_or_stream
         serialize(*parse(**prepare(file_or_stream, outpath, stream),
                          infmt=infmt),
                   debug=debug, profile=profile)
@@ -135,11 +137,16 @@ def convert(file, outpath=None, stream=False,
         # file list is used here because this allows is to merge files
         # without any additional code if we pass it more than one file
         # in normal use a tuple with a single element is passed in
-        file_list = file
-        graph = rdflib.Graph()
-        [parse(**prepare(file, outpath), graph=graph, infmt=infmt) for file in file_list]
-        serialize(graph, outpath, outfmt=outfmt,
-                  debug=debug, profile=profile)
+        file_list = file_or_list_or_stream
+        if outpath is not None:
+            graph = graph_class()
+            [parse(**prepare(file, outpath), graph=graph, infmt=infmt) for file in file_list]
+            serialize(graph, outpath, outfmt=outfmt,
+                      debug=debug, profile=profile)
+        else:
+            [convert(file, infmt=infmt, outfmt=outfmt,
+                     debug=debug, profile=profile,
+                     graph_class=graph_class) for file in file_list]
 
 
 def pipe_debug(*args, source=None, graph=None, outpath=None, **kwargs):
@@ -199,6 +206,7 @@ def main():
         if outpath or args['--slow'] or lenfiles == 1:
             if lenfiles == 1:
                 files,  = files
+
             convert(files, outpath=outpath,
                     infmt=infmt, outfmt=outfmt,
                     debug=debug, profile=profile)
