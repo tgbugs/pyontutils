cc6c4c5fa73dc87218230698183d4ec94abd7fc4
tmsincomb <troysincomb@gmail.com>
2019-10-24 08:22:58 -0700

    update

--- ilxutils/ilxutils/remotes.py
+++ ilxutils/ilxutils/remotes.py
@@ -2,19 +2,19 @@ import ontquery as oq
 from pyontutils.core import OntTerm
 import os
 
-InterLexRemote = oq.plugin.get('InterLex')
-
 TEST = 'https://test3.scicrunch.org/api/1/'
 PRODUCTION = 'https://scicrunch.org/api/1/'
 
+InterLexRemote = oq.plugin.get('InterLex')
 interlex_remote_production = InterLexRemote(
     # When ready, should be changed to 'https://scicrunch.org/api/1/' for production
     apiEndpoint = PRODUCTION
 )
 interlex_remote_production.setup(instrumented=OntTerm)
 
-interlex_remote_test = InterLexRemote(
-    # When ready, should be changed to 'https://scicrunch.org/api/1/' for production
-    apiEndpoint = TEST
-)
-interlex_remote_test.setup(instrumented=OntTerm)
+# InterLexRemote = oq.plugin.get('InterLex')
+# interlex_remote_test = InterLexRemote(
+#     # When ready, should be changed to 'https://scicrunch.org/api/1/' for production
+#     apiEndpoint = TEST
+# )
+# interlex_remote_test.setup(instrumented=OntTerm)
