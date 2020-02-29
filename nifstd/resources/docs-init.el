87bfe681fc94e6332254c6dfb30ee65685dac64a
Tom Gillespie <tgbugs@gmail.com>
2020-02-28 20:22:38 -0800

    docs-init.el prevent rpm spec from prepending blocks with garbage

--- nifstd/resources/docs-init.el
+++ nifstd/resources/docs-init.el
@@ -2,3 +2,6 @@
 
 ;; set the default theme for rendering source blocks
 (load-theme 'whiteboard t)  ; the t must be present
+
+;; prevent rpm-spec-mode from inserting a blank template during export
+(setq rpm-spec-initialize-sections nil)
