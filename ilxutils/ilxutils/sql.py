a6c9c77471479a40447548a7da5d09f879467b83
Troy Sincomb <troysincomb@gmail.com>
2020-12-10 05:34:55 -0800

    added proper test port

--- ilxutils/ilxutils/sql.py
+++ ilxutils/ilxutils/sql.py
@@ -4,13 +4,9 @@ import os
 def production_sql(from_backup=True):
     return IlxSql(db_url=os.environ.get('SCICRUNCH_DB_URL_PRODUCTION'), from_backup=from_backup)
 
-def beta_sql(from_backup=True):
-    # TEST{#} should be a thing since this still relies on main sql test
-    return IlxSql(db_url=os.environ.get('SCICRUNCH_DB_URL_BETA'), from_backup=from_backup)
-
-# entities = []
-# for ilx, group in ex.groupby('ilx'):
-#     if not any(list(group['preferred'] == '1')):
-#         entities.append(ilx)
-
-# from ontquery.plugins.services.interlex_client import InterLexClient
+def test_sql(db_url=None, schema=None, from_backup=True):
+    db_url = os.environ.get('SCICRUNCH_DB_URL_TEST') if not db_url else  db_url
+    # Default test schema is nif_test3
+    if schema:
+        db_url = '/'.join(db_url.split('/')[:-1] + [schema])
+    return IlxSql(db_url=db_url, from_backup=from_backup)
