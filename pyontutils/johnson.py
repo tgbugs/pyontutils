102841bb797b1522148656c6db9226d8b4b5764c
Tom Gillespie <tgbugs@gmail.com>
2024-12-11 00:14:57 -0800

    johnson updated scc to use the iter version from mihali's fork
    the recursive version failed with a recursion error on the big cycles,
    the py-tarjan impl can also be used as a drop in replacement and the
    performance is essentially undistinguishable, however keeping deps low

--- pyontutils/johnson.py
+++ pyontutils/johnson.py
@@ -8,6 +8,7 @@
 
 from collections import defaultdict
 
+
 def simple_cycles(G):
     # Yield every elementary cycle in python graph G exactly once
     # Expects a dictionary mapping from vertices to iterables of vertices
@@ -56,7 +57,47 @@ def simple_cycles(G):
         H = subgraph(G, set(scc))
         sccs.extend(strongly_connected_components(H))
 
+
 def strongly_connected_components(graph):
+    return list(strongly_connected_components_iter(graph))
+
+
+def strongly_connected_components_iter(graph):
+
+    identified = set()
+    stack = []
+    index = {}
+    boundaries = []
+
+    for v in graph:
+        if v not in index:
+            to_do = [('VISIT', v)]
+            while to_do:
+                operation_type, v = to_do.pop()
+                if operation_type == 'VISIT':
+                    index[v] = len(stack)
+                    stack.append(v)
+                    boundaries.append(index[v])
+                    to_do.append(('POSTVISIT', v))
+                    to_do.extend(
+                        reversed([('VISITEDGE', w) for w in graph[v]]))
+                elif operation_type == 'VISITEDGE':
+                    if v not in index:
+                        to_do.append(('VISIT', v))
+                    elif v not in identified:
+                        while index[v] < boundaries[-1]:
+                            boundaries.pop()
+                else:
+                    # operation_type == 'POSTVISIT'
+                    if boundaries[-1] == index[v]:
+                        boundaries.pop()
+                        scc = set(stack[index[v]:])
+                        del stack[index[v]:]
+                        identified.update(scc)
+                        yield scc
+
+
+def strongly_connected_components_rec(graph):
     # Tarjan's algorithm for finding SCC's
     # Robert Tarjan. "Depth-first search and linear graph algorithms." SIAM journal on computing. 1972.
     # Code by Dries Verdegem, November 2012
@@ -96,6 +137,7 @@ def strongly_connected_components(graph):
             _strong_connect(node)
 
     return result
+
 
 def remove_node(G, target):
     # Completely remove a node from the graph
